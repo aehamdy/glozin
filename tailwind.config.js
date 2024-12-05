394047d57afa0199fc65b9abadadf9077d93adad
aehamdy <ae.hamdy91@gmail.com>
2024-12-06 00:07:35 +0200

    Feat: Add project color palette to tailwind CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,34 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        announcementBar: '#607890',
+        primary: {
+          dark: '#000',
+          light: '#fff',
+        },
+        secondary: {
+          dark: '#111',
+        },
+        button: {
+          buyNow: '#d0473e',
+        },
+        price: {
+          originalPrice: '##666',
+          salePrice: '#d0473e',
+        },
+        text: {
+          heading: '#111',
+          content: '#444',
+        },
+        badge: {
+          notification: '#d0473d',
+          sale: '#d0473e',
+          limitedStock: '#14854f',
+        }
+      }
+    },
   },
   plugins: [],
 }
