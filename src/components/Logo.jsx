fb6b85af631423e65c6b308d42894e3dd9a98a25
aehamdy <ae.hamdy91@gmail.com>
2024-12-06 20:05:00 +0200

    Feat: Add Logo component to display logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import logoImg from "../assets/logo.svg"
+
+function Logo() {
+    return (
+        <div>
+            <img src={logoImg} alt="logo image" />
+        </div>
+    )
+}
+
+export default Logo;
