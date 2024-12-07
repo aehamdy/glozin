892e52039be5cdf7c2d3411989f4d5f76511b0e6
aehamdy <ae.hamdy91@gmail.com>
2024-12-07 15:04:26 +0200

    Feat: Add navData.js file that contains all nav list items

---
+++ src/data/navData.js
@@ -0,0 +1,24 @@
+const navData = [
+    {
+        label: "home",
+        url: "/",
+    },
+    {
+        label: "shop",
+        url: "/shop",
+    },
+    {
+        label: "home",
+        url: "/",
+    },
+    {
+        label: "contact",
+        url: "/contact-us",
+    },
+    {
+        label: "about",
+        url: "/about-us",
+    },
+];
+
+export default navData;
