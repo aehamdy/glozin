fa73e37da5b8fa92b15d932abe5856dfe0273661
aehamdy <ae.hamdy91@gmail.com>
2024-12-09 17:28:56 +0200

    Feat: Add slide-in and fade-out animation to 'Nav' component on small screens

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,29 +4,32 @@ import NavHeading from "./NavHeading";
 import NavList from "./NavList";
 
 function Nav({ navVisibility, onClickFunction }) {
+
   return (
     <>
       {/* For small and medium screens */}
-      {navVisibility && (
-        <nav
-          className="lg:hidden absolute top-0 start-0 flex-col items-start w-2/3 h-full bg-primary-light z-50 rounded-tr-2xl rounded-br-2xl overflow-hidden"
-          role="menu"
-          aria-expanded={navVisibility}
-        >
-          <div className="flex flex-col">
-            <div className="flex justify-between items-center py-4 px-3 bg-primary-red">
-              <NavHeading />
-              <Icon
-                name="close"
-                className="hover:text-primary-light"
-                onClickFunction={onClickFunction}
-              />
-            </div>
-
-            <NavList />
+      <nav
+        className={`lg:hidden absolute top-0 left-0 flex-col items-start w-2/3 h-full bg-primary-light z-50 rounded-tr-2xl rounded-br-2xl overflow-hidden transition-all duration-[350ms] ease-in-out transform ${
+          navVisibility
+            ? "translate-x-0 opacity-100"
+            : "-translate-x-full opacity-0"
+        }`}
+        role="menu"
+        aria-expanded={navVisibility}
+      >
+        <div className="flex flex-col">
+          <div className="flex justify-between items-center py-4 px-3 bg-primary-red">
+            <NavHeading />
+            <Icon
+              name="close"
+              className="hover:text-primary-light"
+              onClickFunction={onClickFunction}
+            />
           </div>
-        </nav>
-      )}
+
+          <NavList navVisibility={navVisibility} />
+        </div>
+      </nav>
 
       {/* For large screens */}
       <nav role="menu" className="hidden lg:block">
@@ -35,4 +38,5 @@ function Nav({ navVisibility, onClickFunction }) {
     </>
   );
 }
+
 export default Nav;
--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -2,13 +2,16 @@ import navData from "../data/navData";
 import NavListItem from "./NavListItem";
 
 function NavList() {
-    const renderNavItems = () => {
-        return navData.map((item, index) => <NavListItem key={index} item={item} />)
-    }
+  const renderNavItems = () => {
+    return navData.map((item, index) => (
+      <NavListItem key={index} item={item} />
+    ));
+  };
+
   return (
     <ul className="flex flex-col lg:flex-row lg:justify-between items-start lg:items-center lg:gap-3 divide-y lg:divide-none py-3 px-3 lg:p-0">
-    {renderNavItems()}
-  </ul>
-  )
+      {renderNavItems()}
+    </ul>
+  );
 }
-export default NavList
+export default NavList;
