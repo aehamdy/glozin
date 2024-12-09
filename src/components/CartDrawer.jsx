4111a6ef0753df7c6c92c76754c9ef842acb621c
aehamdy <ae.hamdy91@gmail.com>
2024-12-09 21:20:23 +0200

    Refactor: Adjust CartDrawer width on different screens

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -3,7 +3,7 @@ import Icon from "./Icon";
 
 function CartDrawer() {
   return (
-    <div className="absolute top-0 end-0 w-full h-full lg:w-2/5 flex flex-col py-4 px-4 bg-primary-light">
+    <div className="absolute top-0 end-0 w-10/12 md:w-1/2 lg:w-2/5 h-full flex flex-col py-4 px-4 bg-primary-light">
       <div className="flex justify-between items-center">
         <h4 className="font-semibold text-xl text-heading-dark">
           Shopping Cart (0)
