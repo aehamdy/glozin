d8792ec40b7f7ea96e1811b10f9e9ab138b310dd
aehamdy <ae.hamdy91@gmail.com>
2024-12-09 18:17:58 +0200

    Feat: Replace some icons with better icons according to UI icons

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,27 +1,31 @@
 /* eslint-disable react/prop-types */
-import { CiHeart, CiSearch } from "react-icons/ci"
+import { FiHeart, FiSearch, FiShoppingBag } from "react-icons/fi";
 import { IoCloseOutline } from "react-icons/io5";
-import { LiaShoppingBagSolid } from "react-icons/lia";
-import { PiUser } from "react-icons/pi"
+import { PiUser } from "react-icons/pi";
 import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const iconMap = {
   menu: RxHamburgerMenu,
   close: IoCloseOutline,
-  search: CiSearch,
+  search: FiSearch,
   user: PiUser,
-  wishlist: CiHeart,
-  cart: LiaShoppingBagSolid,
+  wishlist: FiHeart,
+  cart: FiShoppingBag,
   rightArrow: RiArrowRightSLine,
   leftArrow: RiArrowLeftSLine,
-}
+};
 
-function Icon({ name, size = 24, className="", onClickFunction }) {
-
+function Icon({ name, size = 20, className = "", onClickFunction }) {
   const IconComponent = iconMap[name];
   const defaultClasses = "text-secondary-dark cursor-pointer duration-medium";
 
-  return <IconComponent size={size} className={`${defaultClasses} ${className}`} onClick={onClickFunction} />
+  return (
+    <IconComponent
+      size={size}
+      className={`${defaultClasses} ${className}`}
+      onClick={onClickFunction}
+    />
+  );
 }
-export default Icon
+export default Icon;
