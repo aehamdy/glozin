460b3357e30c6ca9dfb95c1d8ef26fe9b484724f
aehamdy <ae.hamdy91@gmail.com>
2024-12-09 19:29:44 +0200

    Feat: Add EmptyCartHeading that represents the heading text when cart is empty

--- src/components/AnnouncementBar.jsx
+++ src/components/AnnouncementBar.jsx
@@ -4,7 +4,7 @@ import Icon from "./Icon";
 function AnnouncementBar({ hideAnnouncementBar }) {
   return (
     <div
-      className={`absolute w-full start-0 top-0 flex gap-4 justify-center items-center py-1 text-primary-light bg-muted-blue`}
+      className={`absolute w-full start-0 top-0 flex gap-4 justify-center items-center py-1 text-sm text-primary-light bg-muted-blue`}
     >
       <p className="">
         🔥 Don&apos;t miss our White Friday <span>Sale 75% Off</span>
---
+++ src/components/EmptyCartHeading.jsx
@@ -0,0 +1,8 @@
+function CartDrawer() {
+  return (
+    <h6 className="font-semibold text-2xl text-primary-dark">
+      Your cart is currently empty!.
+    </h6>
+  );
+}
+export default CartDrawer;
