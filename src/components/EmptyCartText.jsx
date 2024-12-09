cde551836f6d6b737dc01728bebff29246704518
aehamdy <ae.hamdy91@gmail.com>
2024-12-09 19:33:21 +0200

    Feat: Add EmptyCartText that represents the cart is empty

---
+++ src/components/EmptyCartText.jsx
@@ -0,0 +1,8 @@
+function EmptyCartText() {
+  return (
+    <p className="max-w-[85%] text-primary-dark">
+      You may checkout all the available products and buy some in the shop.
+    </p>
+  );
+}
+export default EmptyCartText;
