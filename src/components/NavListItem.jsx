7fe6d7dcf6339f0be159640d828aaf6ca524d347
aehamdy <ae.hamdy91@gmail.com>
2024-12-10 16:26:38 +0200

    Feat: Adjust NavListItem color to be dark, but red if its content is 'sale' or 'clearance'

--- src/components/NavListItem.jsx
+++ src/components/NavListItem.jsx
@@ -6,7 +6,12 @@ function NavListItem({ item }) {
     <li
       className={`relative before:absolute before:start-0 before:bottom-0 before:h-[2px] before:w-0 lg:hover:before:w-full 
         before:bg-secondary-red flex justify-between w-full py-4 lg:py-2 lg:px-2 font-semibold 
-        text-secondary-dark hover:text-secondary-red lg:border-none cursor-pointer duration-medium before:duration-medium`}
+        ${
+          item.label.toLowerCase() === "sale" ||
+          item.label.toLowerCase() === "clearance"
+            ? "text-primary-red hover:text-secondary-red"
+            : "text-secondary-dark hover:text-secondary-red"
+        } lg:border-none cursor-pointer duration-medium before:duration-medium`}
     >
       <div className="flex relative">
         {item.label.charAt(0).toUpperCase() + item.label.slice(1).toLowerCase()}
