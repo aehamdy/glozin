caefeb7e89d01d0d2d61f4f465ce35a517f89a55
aehamdy <ae.hamdy91@gmail.com>
2024-12-11 22:24:29 +0200

    Feat: Add NavigationDot component to represent navigation dots

---
+++ src/components/NavigationDot.jsx
@@ -0,0 +1,6 @@
+function NavigationDot() {
+  return (
+    <span className="relative w-5 h-5 border border-white rounded-full backdrop-blur-sm before:absolute before:w-[5px] before:h-[5px] before:top-1/2 before:start-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-navigation-dots before:rounded-full"></span>
+  );
+}
+export default NavigationDot;
--- src/components/NavigationDotsWrapper.jsx
+++ src/components/NavigationDotsWrapper.jsx
@@ -1,12 +1,11 @@
+import NavigationDot from "./NavigationDot";
+
 /* eslint-disable react/prop-types */
 function NavigationDotsWrapper({ count }) {
   return (
     <div className="absolute flex gap-3 w-fit bottom-3 py-1 px-2 rounded-small backdrop-blur-sm">
       {Array.from({ length: count }).map((_, index) => (
-        <span
-          key={index}
-          className="relative w-5 h-5 border border-white rounded-full backdrop-blur-sm before:absolute before:w-[5px] before:h-[5px] before:top-1/2 before:start-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:bg-navigation-dots before:rounded-full"
-        ></span>
+        <NavigationDot key={index} />
       ))}
     </div>
   );
