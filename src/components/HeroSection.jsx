9eac5dddd0e8ceea39b0df98e0279347ec40122d
aehamdy <ae.hamdy91@gmail.com>
2024-12-11 20:02:54 +0200

    Feat: Add slider functinality to change between slides in HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import banner1 from "../assets/hero-banner-1.jpg";
 import banner2 from "../assets/hero-banner-2.jpg";
 import banner3 from "../assets/hero-banner-3.jpg";
@@ -9,11 +10,33 @@ const slides = [
 ];
 
 function HeroSection() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % slides.length);
+  };
+
+  const previousSlide = () => {
+    setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
+  };
+
   return (
-    <section className="relative w-full h-dvh bg-sky-800 rounded-small overflow-hidden">
-      <div>
-        <img src="" alt="" className="w-full" />
+    <section className="relative flex justify-center items-center w-full h-[100dvh - 65px] mt-10 bg-sky-800 rounded-small overflow-hidden">
+      <div className="w-full h-full">
+        <img src={slides[currentSlide].image} alt="" className="w-full" />
       </div>
+      <span
+        onClick={previousSlide}
+        className="absolute start-0 w-4 h-4 bg-emerald-400"
+      >
+        &#8592;
+      </span>
+      <span
+        onClick={nextSlide}
+        className="absolute end-0 w-4 h-4 bg-emerald-400"
+      >
+        &#8594;
+      </span>
     </section>
   );
 }
