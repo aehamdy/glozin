98b20cc0c91656f400714846220805c7a654f18e
aehamdy <ae.hamdy91@gmail.com>
2024-12-11 18:19:01 +0200

    Feat: Add HeroSection slide images

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,3 +1,13 @@
+import banner1 from "../assets/hero-banner-1.jpg";
+import banner2 from "../assets/hero-banner-2.jpg";
+import banner3 from "../assets/hero-banner-3.jpg";
+
+const slides = [
+  { id: 1, image: banner1, subHeading: "", heading: "" },
+  { id: 2, image: banner2, subHeading: "", heading: "" },
+  { id: 3, image: banner3, subHeading: "", heading: "" },
+];
+
 function HeroSection() {
   return (
     <section className="relative w-full h-dvh bg-sky-800 rounded-small overflow-hidden">
