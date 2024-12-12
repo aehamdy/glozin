ae6a5c40a88ecd54b77350f09eace3e9c158be5e
aehamdy <ae.hamdy91@gmail.com>
2024-12-12 20:10:11 +0200

    Feat: Create SpotlightOfferSection component

---
+++ src/components/SpotlightOfferSection.jsx
@@ -0,0 +1,25 @@
+const banners = [
+  {
+    id: 1,
+    subHeading: "Save 30-50% Blazers",
+    heading: "Colour Spotlight",
+    textContent: "In-store! Limited time offer",
+  },
+  {
+    id: 2,
+    subHeading: "Sale up to 75% off",
+    heading: "Coats Collection",
+    textContent: "From $175. Do not miss!",
+  },
+  {
+    id: 3,
+    subHeading: "Save 10-30% Dresses",
+    heading: "Everyday Luxury",
+    textContent: "In-store! Limited time offer",
+  },
+];
+
+function SpotlightOfferSection() {
+  return <section className="grid grid-col-1 lg:grid-cols-3"></section>;
+}
+export default SpotlightOfferSection;
