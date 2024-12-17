bf11c62b4897543981af4f3073233ba68ee66b2d
aehamdy <ae.hamdy91@gmail.com>
2024-12-17 19:57:04 +0200

    Feat: Create FooterCreatedBy component

---
+++ src/components/FooterCreatedBy.jsx
@@ -0,0 +1,15 @@
+function FooterCreatedBy() {
+  return (
+    <p>
+      Created by{" "}
+      <a
+        href="https://ahmedessam-portfolio.vercel.app/"
+        target="_blank"
+        className="font-bold text-muted-blue"
+      >
+        AE.DEV
+      </a>
+    </p>
+  );
+}
+export default FooterCreatedBy;
