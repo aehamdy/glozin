20f683f6b70f108ee17455d5cf69d25fbbb85ea6
aehamdy <ae.hamdy91@gmail.com>
2024-12-23 00:04:38 +0200

    Feat: Create OverviewWrapper component

---
+++ src/assets/OverviewWrapper.jsx
@@ -0,0 +1,23 @@
+const overviews = [
+  {
+    image: "",
+    heading: "Our Factories",
+    subheading: "The Best Product",
+    descritpion:
+      "We spend a lot of time finding the best factories around the world—the same factories that make your favorite designer brands. We visit them regularly and build strong personal relationships with the owners. Each factory is audited for compliance to assess factors such as fair wages.",
+    buttonText: "Learn More",
+  },
+  {
+    image: "",
+    heading: "Our Products",
+    subheading: "Timeless Products",
+    descritpion:
+      "At Glozin, we’re not big on trends. We want you to wear our pieces for years, even decades, to come. That’s why we source the finest materials and factories for our timeless products— like our Grade-A cashmere sweaters, Italian shoes, and Peruvian Pima tees.",
+    buttonText: "Learn More",
+  },
+];
+
+function OverviewWrapper() {
+  return <div>{overviews}</div>;
+}
+export default OverviewWrapper;
