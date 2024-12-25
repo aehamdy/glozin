597e1ee680a61c8e5518d87f69089146453b424d
aehamdy <ae.hamdy91@gmail.com>
2024-12-25 21:50:33 +0200

    Refactor: Implement storeData in FooterCopyright component

--- src/components/FooterCopyright.jsx
+++ src/components/FooterCopyright.jsx
@@ -1,6 +1,15 @@
+import storeData from "../data/storeData";
+
 function FooterCopyright() {
   const d = new Date();
   const year = d.getFullYear();
-  return <p>&copy; {year} VogueCart store. All rights reserved.</p>;
+  return (
+    <p>
+      &copy; {year}{" "}
+      {storeData.name.charAt(0).toUpperCase() +
+        storeData.name.slice(1).toLowerCase()}{" "}
+      store. All rights reserved.
+    </p>
+  );
 }
 export default FooterCopyright;
