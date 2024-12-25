af85a53bcca37a657266e843a992201fe6472ad8
aehamdy <ae.hamdy91@gmail.com>
2024-12-25 21:41:19 +0200

    Feat: Create storeData file that contains name, numbers and emails of the store

---
+++ src/data/storeData.js
@@ -0,0 +1,9 @@
+const storeData = {
+    name: "glozin",
+    phone: "+1 (973) 435-3638",
+    customerCareEmail: "info@fashionwomen.com",
+    saleEmail: "sale@fashionwomen.com",
+    pressEmail: "press@fashionwomen.com",
+};
+
+export default storeData;
