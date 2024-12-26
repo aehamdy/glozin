a36f08af9bff70adc37bc6945a54a009b51f1f3e
aehamdy <ae.hamdy91@gmail.com>
2024-12-26 19:07:56 +0200

    Refactor: change Accordion elements to implement animation to the accordion when it opens/closes

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,28 +1,38 @@
 /* eslint-disable react/prop-types */
-
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Icon from "./Icon";
 
 function Accordion({ question, answer }) {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
 
-  const handleToggle = (e) => {
-    setIsOpen(e.target.open);
+  const handleToggle = () => {
+    setIsOpen((prev) => !prev);
   };
 
   return (
-    <details onToggle={handleToggle} className="mb-5">
-      <summary className="flex justify-between gap-10 w-fit py-3 font-semibold text-secondary-dark cursor-pointer">
+    <div className="mb-5 pb-3 border-b">
+      <button
+        className="flex justify-between gap-10 w-full py-3 font-semibold text-secondary-dark"
+        onClick={handleToggle}
+        aria-expanded={isOpen}
+      >
         {question}
-        <Icon
-          name={isOpen ? "minus" : "plus"}
-          className="text-content-medium-dark"
-        />
-      </summary>
-      <div className="px-5 text-content-medium-dark">
-        <p>{answer}</p>
+        <Icon name={isOpen ? "minus" : "plus"} />
+      </button>
+      <div
+        ref={contentRef}
+        className="overflow-hidden transition-max-height duration-300 ease-in-out"
+        style={{
+          maxHeight: isOpen ? `${contentRef.current.scrollHeight}px` : "0",
+        }}
+      >
+        <div className="pb-5 px-5 text-content-medium-dark">
+          <p>{answer}</p>
+        </div>
       </div>
-    </details>
+    </div>
   );
 }
+
 export default Accordion;
