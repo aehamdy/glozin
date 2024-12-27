d4d2b5dc3f762b883ad9356069c4cc786df263dd
aehamdy <ae.hamdy91@gmail.com>
2024-12-27 16:14:39 +0200

    Fix: Resolve the issue of scroll behavior by integrating ScrollToTop into PageLayout for route-based re-rendering

--- src/pages/PageLayout.jsx
+++ src/pages/PageLayout.jsx
@@ -4,6 +4,7 @@ import CartDrawer from "../components/CartDrawer";
 import Footer from "../components/Footer";
 import FooterBottomBar from "../components/FooterBottomBar";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../navigation/ScrollToTop";
 
 function PageLayout() {
   const [isCartOpen, setIsCartOpen] = useState(false);
@@ -12,13 +13,14 @@ function PageLayout() {
   const handleCloseCart = () => setIsCartOpen(false);
 
   return (
-    <div>
+    <>
+      <ScrollToTop />
       <Header handleOpenCart={handleOpenCart} />
       <CartDrawer isCartOpen={isCartOpen} handleCloseCart={handleCloseCart} />
       <Outlet />
       <Footer />
       <FooterBottomBar />
-    </div>
+    </>
   );
 }
 export default PageLayout;
