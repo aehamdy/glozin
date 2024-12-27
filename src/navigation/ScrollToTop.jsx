3713a5ebfdd6899064d14c435e7b05c56128a90c
aehamdy <ae.hamdy91@gmail.com>
2024-12-27 16:18:11 +0200

    Refactor: Refactor ScrollToTop component implementation to ensure proper functionality by integrating it within the layout structure

--- src/navigation/ScrollToTop.jsx
+++ src/navigation/ScrollToTop.jsx
@@ -7,19 +7,25 @@ import { useLocation } from "react-router-dom";
  * to a new route. By listening to route changes via React Router's `useLocation` hook,
  * it triggers a scroll reset to provide a consistent user experience.
  *
+ * To work as expected, this component should be placed within a layout or wrapper
+ * that gets re-rendered on route changes (e.g., `PageLayout` in this application).
+ * Adding it directly in the `RouterProvider` will not trigger the desired behavior
+ * because the provider does not re-render on route changes.
+ *
  * Without this component, navigating to a new page might retain the previous scroll
  * position, which could be confusing or undesirable for users.
  *
  * Usage:
- * - Import and include the `ScrollToTop` component at the root of your application,
- *   typically within the main layout or router configuration.
+ * - Import and include the `ScrollToTop` component within the layout or wrapper
+ *   responsible for managing route changes.
  *
  * Example:
  * ```jsx
- * <BrowserRouter>
+ * <PageLayout>
  *   <ScrollToTop />
- *   <App />
- * </BrowserRouter>
+ *   <Outlet />
+ * </PageLayout>
+ * ```
  */
 
 function ScrollToTop() {
