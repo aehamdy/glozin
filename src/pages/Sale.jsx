db4993443a91dd02277e38f87f43e97b788c570c
aehamdy <ae.hamdy91@gmail.com>
2024-12-30 22:31:14 +0200

    Feat: Add sale component heading and paragraph to Sale component

--- src/pages/Sale.jsx
+++ src/pages/Sale.jsx
@@ -6,6 +6,16 @@ function Sale() {
       <div className="relative before:absolute before:w-full before:h-full before:top-0 before:start-0 before:bg-[#a91717] before:opacity-70 z-[-1]">
         <video autoPlay loop muted src={saleVideo}></video>
       </div>
+      <div className="absolute flex flex-col items-center gap-5 text-primary-light top-[10%] start-1/2 -translate-x-1/2 z-[1]">
+        <h2 className="font-bold text-6xl tracking-tight">
+          White Friday
+          <br />
+          Sale Up To 75% Off
+        </h2>
+        <p className="text-lg">
+          White Friday sale up to 75% all items. Hurry up!
+        </p>
+      </div>
     </section>
   );
 }
