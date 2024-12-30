6b0f9cf969674fa350ebeac522bf434a9a3c0efd
aehamdy <ae.hamdy91@gmail.com>
2024-12-30 22:30:07 +0200

    Feat: Add sale page video and its overlay to Sale component

--- src/pages/Sale.jsx
+++ src/pages/Sale.jsx
@@ -1,4 +1,12 @@
+import saleVideo from "../assets/sale-video.mp4";
+
 function Sale() {
-  return <section>Sale</section>;
+  return (
+    <section className="relative">
+      <div className="relative before:absolute before:w-full before:h-full before:top-0 before:start-0 before:bg-[#a91717] before:opacity-70 z-[-1]">
+        <video autoPlay loop muted src={saleVideo}></video>
+      </div>
+    </section>
+  );
 }
 export default Sale;
