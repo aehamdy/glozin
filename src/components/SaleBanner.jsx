462250b1c162b3db91451a10818a5d8c5a37ab85
aehamdy <ae.hamdy91@gmail.com>
2024-12-31 16:31:58 +0200

    Style: Add styles to SaleBanner as required in the UI

--- src/components/SaleBanner.jsx
+++ src/components/SaleBanner.jsx
@@ -1,14 +1,16 @@
 function SaleBanner() {
   return (
-    <article>
-      <div>
-        <span>HOLIDAY</span>
-        <h4>Sale 30% Off</h4>
-        <p>30% off during the hoilday season.</p>
+    <article className="flex flex-col gap-[2px]">
+      <div className="flex flex-col items-center gap-2 pt-16 pb-8 px-6 text-primary-light bg-[#fff3] border border-[#fff3] rounded-small backdrop-filter">
+        <span className="w-fit py-2 px-5 font-semibold text-xs text-secondary-dark bg-primary-light rounded-medium">
+          HOLIDAY
+        </span>
+        <h4 className="font-bold text-2xl">Sale 30% Off</h4>
+        <p className="text-[15px]">30% off during the hoilday season.</p>
       </div>
-      <div>
-        <p>USE CODE:</p>
-        <h5>HOLIDAY30ALL</h5>
+      <div className="py-5 px-5 text-secondary-dark bg-primary-light rounded-small">
+        <p className="text-sm">USE CODE:</p>
+        <h5 className="font-extraboldl">HOLIDAY30ALL</h5>
       </div>
     </article>
   );
