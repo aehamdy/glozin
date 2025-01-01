cbf73570446c32170cbd9db522b43a878de6fa68
aehamdy <ae.hamdy91@gmail.com>
2025-01-01 22:34:36 +0200

    Refactor: Add collection images to collections.js file

--- src/data/collections.js
+++ src/data/collections.js
@@ -1,38 +1,46 @@
+import menCollection from "../assets/category-men.webp";
+import womenCollection from "../assets/category-women.webp";
+import womenDividedCollection from "../assets/category-women-divided.webp";
+import sportsCollection from "../assets/category-sports.webp";
+import kidsCollection from "../assets/category-kids.webp";
+import babyCollection from "../assets/category-baby.webp";
+import homeCollection from "../assets/category-home.webp";
+
 const collections = [
     {
         name: "Men",
-        image: "",
-        link: "/men"
+        image: menCollection,
+        link: "/men-collection"
     },
     {
         name: "Women",
-        image: "",
-        link: "/women"
+        image: womenCollection,
+        link: "/women-collection"
     },
     {
         name: "Women Divided",
-        image: "",
-        link: "/womendivided"
+        image: womenDividedCollection,
+        link: "/women-divided-collection"
     },
     {
         name: "Sports Wear",
-        image: "",
-        link: "/sportswear"
+        image: sportsCollection,
+        link: "/sportswear-collection"
     },
     {
         name: "Kids",
-        image: "",
-        link: "/kids"
+        image: kidsCollection,
+        link: "/kids-collection"
     },
     {
         name: "Baby",
-        image: "",
-        link: "/baby"
+        image: babyCollection,
+        link: "/baby-collection"
     },
     {
         name: "Home",
-        image: "",
-        link: "/home"
+        image: homeCollection,
+        link: "/home-collection"
     },
 ];
 
