503f7e0858ed964cc4f29460a3f9022636631e6a
aehamdy <ae.hamdy91@gmail.com>
2025-01-01 22:18:14 +0200

    Feat: Create collections.js file that contains all categories of the shop

--- src/components/CollectionsWrapper.jsx
+++ src/components/CollectionsWrapper.jsx
@@ -1,7 +1,7 @@
 function CollectionsWrapper() {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      Collections Wrapper
+      Hello There!
     </div>
   );
 }
---
+++ src/data/collections.js
@@ -0,0 +1,39 @@
+const collections = [
+    {
+        name: "Men",
+        image: "",
+        link: "/men"
+    },
+    {
+        name: "Women",
+        image: "",
+        link: "/women"
+    },
+    {
+        name: "Women Divided",
+        image: "",
+        link: "/womendivided"
+    },
+    {
+        name: "Sports Wear",
+        image: "",
+        link: "/sportswear"
+    },
+    {
+        name: "Kids",
+        image: "",
+        link: "/kids"
+    },
+    {
+        name: "Baby",
+        image: "",
+        link: "/baby"
+    },
+    {
+        name: "Home",
+        image: "",
+        link: "/home"
+    },
+];
+
+export default collections;
