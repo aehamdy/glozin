6643b46955fa51c51f97aba324ddbf3a2baec997
aehamdy <ae.hamdy91@gmail.com>
2025-01-07 19:14:41 +0200

    Feat: Add 'Terms & Conditions' checkbox, and 'Buy Now' button

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -129,7 +129,7 @@ function ProductDetails() {
             </div>
           </div>
 
-          <div className="product-form flex flex-col gap-3 w-full mt-5 pt-8 border-t">
+          <div className="product-form flex flex-col gap-5 w-full my-5 pt-8 border-t">
             <div className="flex justify-between items-center px-1">
               <div className="flex items-center bg-[#F5F5F5] border rounded-medium overflow-hidden">
                 <button
@@ -166,6 +166,22 @@ function ProductDetails() {
                   className="text-secondary-dark group-hover:text-primary-light group-hover:fill-secondar-dark"
                 />
               </div>
+            </div>
+
+            <div className="flex flex-col gap-4 px-1">
+              <div className="flex items-cetner gap-4">
+                <input type="checkbox" name="" id="" className="" />
+                <p className="flex gap-1 text-secondary-dark">
+                  I agree with{" "}
+                  <span className="font-semibold text-secondary-dark">
+                    Terms & Conditions
+                  </span>
+                </p>
+              </div>
+              <Button
+                value="Buy Now"
+                className={`text-primary-light bg-red-300 py-2.5 rounded-medium`}
+              />
             </div>
           </div>
 
@@ -239,7 +255,7 @@ function ProductDetails() {
             </div>
           </div>
 
-          <div className="mt-2 px-3 text-start bg-gray-50 rounded-tiny">
+          <div className="my-10 px-3 text-start bg-gray-50 rounded-tiny">
             <Accordion
               question={shopConfig.shippingDetails.title}
               answer={shopConfig.shippingDetails.content}
