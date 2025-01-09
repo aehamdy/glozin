331e34f9c7f909639409a9efe5029a9b85c711d0
aehamdy <ae.hamdy91@gmail.com>
2025-01-09 11:38:12 +0200

    Refactor: Get rid of Modal content except children to fully control content and to make Modal deal with children easier

--- src/common/Modal.jsx
+++ src/common/Modal.jsx
@@ -1,25 +1,13 @@
 /* eslint-disable react/prop-types */
 function Modal({ isOpen, onClose, children }) {
-  !isOpen && null;
+  if (!isOpen) return null;
 
   return (
     <div
       className="flex justify-center items-center fixed inset-0 bg-black bg-opacity-50 z-[100]"
       onClick={onClose}
     >
-      <div
-        className="relative max-w-lg w-full p-6 bg-white rounded-md shadow-lg"
-        onClick={(e) => e.stopPropagation()}
-      >
-        <button
-          type="button"
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-800"
-          onClick={onClose}
-        >
-          &#10005;
-        </button>
-        {children}
-      </div>
+      {children}
     </div>
   );
 }
--- src/components/ProductTermsAgreement.jsx
+++ src/components/ProductTermsAgreement.jsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+import Modal from "../common/Modal";
+
 function ProductTermsAgreement() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => setIsModalOpen((prev) => !prev);
+
   return (
     <div className="flex items-cetner gap-4">
       <input type="checkbox" name="" id="" className="" />
       <p className="flex gap-1 text-sm md:text-base text-secondary-dark">
         I agree with{" "}
-        <span className="font-semibold text-secondary-dark">
+        <button
+          type="button"
+          className="font-semibold text-secondary-dark"
+          onClick={toggleModal}
+        >
           Terms & Conditions
-        </span>
+        </button>
+        <Modal isOpen={isModalOpen} onClose={toggleModal}></Modal>
       </p>
     </div>
   );
