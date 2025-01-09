39d5df56a5f286a9686305da34b77600d86eb2eb
aehamdy <ae.hamdy91@gmail.com>
2025-01-09 13:14:51 +0200

    Feat: Add close functionality to Modal when press on 'Esc' keyboard key

--- src/common/Modal.jsx
+++ src/common/Modal.jsx
@@ -1,5 +1,18 @@
 /* eslint-disable react/prop-types */
+import { useEffect } from "react";
+
 function Modal({ isOpen, onClose, children }) {
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") onClose();
+    };
+
+    if (isOpen) {
+      document.addEventListener("keydown", handleKeyDown);
+      return () => document.removeEventListener("keydown", handleKeyDown);
+    }
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
 
   return (
