26aa8cb4bba1e3c6b92d340b4bfe3584fa8e31bf
aehamdy <ae.hamdy91@gmail.com>
2025-01-12 19:51:21 +0200

    Refactor: Replaced BlurredOverlay with Modal in CartDrawer for improved overlay handling

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import Modal from "../common/Modal";
 import BlurredOverlay from "./BlurredOverlay";
 import EmptyCart from "./EmptyCart";
 import Icon from "./Icon";
@@ -6,26 +7,28 @@ import Icon from "./Icon";
 function CartDrawer({ isCartOpen, handleCloseCart }) {
   return (
     <>
-      <BlurredOverlay triggerOnClick={handleCloseCart} state={isCartOpen} />
+      {/* <BlurredOverlay triggerOnClick={handleCloseCart} state={isCartOpen} /> */}
 
-      {/* Cart Drawer */}
-      <div
-        className={`fixed top-0 end-0 w-10/12 md:w-3/5 lg:w-2/5 h-full flex flex-col py-4 px-4 bg-primary-light rounded-l-small overflow-hidden z-top transition-all duration-[350ms] ease-in-out transform ${
-          isCartOpen ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <div className="flex justify-between items-center">
-          <h4 className="font-semibold text-xl text-heading-dark">
-            Shopping Cart (0)
-          </h4>
-          <Icon
-            name="close"
-            className="text-content-medium-dark hover:scale-150"
-            onClickFunction={handleCloseCart}
-          />
+      <Modal isOpen={isCartOpen} onClose={handleCloseCart}>
+        {/* Cart Drawer */}
+        <div
+          className={`fixed top-0 end-0 w-10/12 md:w-3/5 lg:w-2/5 h-full flex flex-col py-4 px-4 bg-primary-light rounded-l-small overflow-hidden z-top transition-all duration-[350ms] ease-in-out transform ${
+            isCartOpen ? "translate-x-0" : "translate-x-1/2"
+          }`}
+        >
+          <div className="flex justify-between items-center">
+            <h4 className="font-semibold text-xl text-heading-dark">
+              Shopping Cart (0)
+            </h4>
+            <Icon
+              name="close"
+              className="text-content-medium-dark hover:scale-150"
+              onClickFunction={handleCloseCart}
+            />
+          </div>
+          <EmptyCart handleCloseCart={handleCloseCart} />
         </div>
-        <EmptyCart handleCloseCart={handleCloseCart} />
-      </div>
+      </Modal>
     </>
   );
 }
