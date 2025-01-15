1229ce108b87b744ac2dc810893dbaea6a264200
aehamdy <ae.hamdy91@gmail.com>
2025-01-16 00:42:02 +0200

    Refactor: Simplify AddToWishlistButton by merging desktop and mobile button implementations

--- src/components/AddToWishlistButton.jsx
+++ src/components/AddToWishlistButton.jsx
@@ -23,40 +23,20 @@ function AddToWishlistButton({ productId }) {
   };
 
   return (
-    <>
-      <div
-        onClick={handleAddToWishlist}
-        className={`hidden md:block addtowishlist-button group md:relative p-3 md:p-4 ${
-          isInWishlist ? "bg-secondary-dark" : "bg-primary-light"
-        } hover:bg-secondary-dark border rounded-full cursor-pointer duration-medium`}
-      >
-        <div className="tooltip hidden md:block absolute -top-5 group-hover:-top-7 start-0 -translate-x-1/4 w-24 py-1 text-xs text-primary-light bg-primary-dark rounded-md opacity-0 group-hover:opacity-100 z-[-1] group-hover:z-40 pointer-events-none duration-300">
-          Add to wishlist
-        </div>
-        <Icon
-          name="wishlist"
-          size="18"
-          className={`${
-            isInWishlist ? "text-primary-light" : "text-secondary-dark"
-          } group-hover:text-primary-light group-hover:fill-secondar-dark`}
-        />
-      </div>
-
-      <div
-        onClick={handleAddToWishlist}
-        className={`md:hidden absolute top-0 -translate-y-1/4 end-0 addtowishlist-button group md:relative p-3 md:p-4 ${
-          isInWishlist ? "bg-primary-dark" : "bg-primary-light"
-        } hover:bg-secondary-dark border rounded-full cursor-pointer duration-medium`}
-      >
-        <Icon
-          name="wishlist"
-          size="18"
-          className={`${
-            isInWishlist ? "text-primary-light" : "text-secondary-dark"
-          } group-hover:text-primary-light group-hover:fill-secondary-dark`}
-        />
-      </div>
-    </>
+    <div
+      onClick={handleAddToWishlist}
+      className={`absolute md:relative top-0 -translate-y-1/4 md:-translate-y-0 end-0 p-3 md:p-4  ${
+        isInWishlist ? "bg-primary-dark" : "bg-primary-light"
+      } hover:bg-secondary-dark border rounded-full cursor-pointer duration-medium`}
+    >
+      <Icon
+        name="wishlist"
+        size="18"
+        className={`${
+          isInWishlist ? "text-primary-light" : "text-secondary-dark"
+        } group-hover:text-primary-light group-hover:fill-secondary-dark`}
+      />
+    </div>
   );
 }
 export default AddToWishlistButton;
