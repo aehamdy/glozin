30bc5fdc4388df9b33f86bd288b28e547c5edb1e
aehamdy <ae.hamdy91@gmail.com>
2025-01-16 22:39:57 +0200

    Style: Add hover scaling and opacity effects to header nav items on large screens

--- src/components/NavList.jsx
+++ src/components/NavList.jsx
@@ -9,7 +9,7 @@ function NavList() {
   };
 
   return (
-    <ul className="flex flex-col lg:flex-row lg:justify-between items-start lg:items-center lg:gap-3 divide-y lg:divide-none py-3 px-3 lg:p-0">
+    <ul className="header-nav flex flex-col lg:flex-row lg:justify-between items-start lg:items-center lg:gap-3 divide-y lg:divide-none py-3 px-3 lg:p-0">
       {renderNavItems()}
     </ul>
   );
