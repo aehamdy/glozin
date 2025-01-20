5cd044d4d513231a778b5087bee65a6047afb0d9
aehamdy <ae.hamdy91@gmail.com>
2025-01-20 18:32:22 +0200

    Fix: Resolve the issue of not redirecting to ProductDetails when click on ProductCard by adjusting ProductCardQuickButtons to not fully cover ProductCard

--- src/components/ProductCardQuickButtons.jsx
+++ src/components/ProductCardQuickButtons.jsx
@@ -9,29 +9,27 @@ function ProductCardQuickButtons({
   removeFromWishlist,
 }) {
   return (
-    <div className="absolute top-0 start-0 w-full h-full z-[-1] group-hover:z-50">
-      <div className="relative w-full h-full">
-        <div className="absolute top-5 end-5 lg:-end-2 group-hover:end-5 flex flex-col gap-4 lg:opacity-0 lg:group-hover:opacity-100 transition-all duration-medium">
-          <div className="flex flex-col gap-5">
-            {removeFromWishlist ? (
-              <button
-                onClick={() => handleRemoveFromWishlist(productId)}
-                className="group relative text-content-medium-dark hover:text-red-500 bg-primary-light rounded-full shadow-md"
-              >
-                <Icon name="close" className="m-2" />
-              </button>
-            ) : (
-              <AddToWishlistButton productId={productId} />
-            )}
-          </div>
+    <>
+      <div className="absolute top-5 end-5 lg:-end-2 group-hover:end-5 flex flex-col gap-4 lg:opacity-0 lg:group-hover:opacity-100 group-hover:z-50 transition-all duration-medium">
+        <div className="relative flex flex-col gap-5">
+          {removeFromWishlist ? (
+            <button
+              onClick={() => handleRemoveFromWishlist(productId)}
+              className="group relative text-content-medium-dark hover:text-red-500 bg-primary-light rounded-full shadow-md"
+            >
+              <Icon name="close" className="m-2" />
+            </button>
+          ) : (
+            <AddToWishlistButton productId={productId} />
+          )}
         </div>
-
-        <Button
-          value="Add to Cart"
-          className="hidden lg:block absolute bottom-[20%] group-hover:bottom-[23%] start-1/2 -translate-x-1/2 w-3/5 py-2 text-lg text-primary-light bg-secondary-dark hover:bg-primary-dark rounded-medium opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-medium"
-        />
       </div>
-    </div>
+
+      <Button
+        value="Add to Cart"
+        className="hidden lg:block absolute bottom-[20%] group-hover:bottom-[23%] start-1/2 -translate-x-1/2 w-3/5 py-2 text-lg text-primary-light bg-secondary-dark hover:bg-primary-dark rounded-medium opacity-0 group-hover:opacity-100 group-hover:z-50 transition-all ease-in-out duration-medium"
+      />
+    </>
   );
 }
 export default ProductCardQuickButtons;
