3bfd22f563e9bdde17e43b3b7bb2ab6a887e31ed
aehamdy <ae.hamdy91@gmail.com>
2025-01-20 18:22:16 +0200

    Refactor: Adjust the position of ProdcutCardQuickButtons

--- src/components/ProductCardQuickButtons.jsx
+++ src/components/ProductCardQuickButtons.jsx
@@ -11,14 +11,14 @@ function ProductCardQuickButtons({
   return (
     <div className="absolute top-0 start-0 w-full h-full z-[-1] group-hover:z-50">
       <div className="relative w-full h-full">
-        <div className="absolute top-5 end-5 lg:-end-8 group-hover:end-5 flex flex-col gap-4 lg:opacity-0 lg:group-hover:opacity-100 transition-all duration-medium">
+        <div className="absolute top-5 end-5 lg:-end-2 group-hover:end-5 flex flex-col gap-4 lg:opacity-0 lg:group-hover:opacity-100 transition-all duration-medium">
           <div className="flex flex-col gap-5">
             {removeFromWishlist ? (
               <button
                 onClick={() => handleRemoveFromWishlist(productId)}
-                className="group bg-primary-light rounded-full shadow-md"
+                className="group relative text-content-medium-dark hover:text-red-500 bg-primary-light rounded-full shadow-md"
               >
-                <Icon name="close" className="m-2 text-content-medium-dark" />
+                <Icon name="close" className="m-2" />
               </button>
             ) : (
               <AddToWishlistButton productId={productId} />
@@ -28,7 +28,7 @@ function ProductCardQuickButtons({
 
         <Button
           value="Add to Cart"
-          className="hidden lg:block absolute bottom-0 group-hover:bottom-[23%] start-1/2 -translate-x-1/2 w-3/5 py-2 text-lg text-primary-light bg-secondary-dark hover:bg-red-500 rounded-medium opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-medium"
+          className="hidden lg:block absolute bottom-[20%] group-hover:bottom-[23%] start-1/2 -translate-x-1/2 w-3/5 py-2 text-lg text-primary-light bg-secondary-dark hover:bg-primary-dark rounded-medium opacity-0 group-hover:opacity-100 transition-all ease-in-out duration-medium"
         />
       </div>
     </div>
