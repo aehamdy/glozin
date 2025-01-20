53d5a19118fdfca568776097de7eed4dc5e8d99b
aehamdy <ae.hamdy91@gmail.com>
2025-01-20 21:31:51 +0200

    Style: Display two products on small screens, three on medium screen and four products on large screens

--- src/components/ProductsWrapper.jsx
+++ src/components/ProductsWrapper.jsx
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard";
 
 function ProductsWrapper({ products }) {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-14 gap-x-6 mt-8">
+    <section className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-y-14 gap-x-6 mt-8">
       {products.map((product, index) => {
         const salePrice =
           product.sale - (product.sale * product.discountPercentage) / 100;
