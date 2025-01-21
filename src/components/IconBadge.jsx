f832ce80c3d393453f21f9cdd69ac49bff0b19aa
aehamdy <ae.hamdy91@gmail.com>
2025-01-21 18:04:31 +0200

    Feat: Display IconBadge only if there is at least one item

--- src/components/IconBadge.jsx
+++ src/components/IconBadge.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/prop-types */
 function IconBadge({ value }) {
-  return (
-    <div className="absolute top-2 end-2 -translate-y-full translate-x-full flex justify-center items-center w-4 h-4 text-xs text-primary-light bg-red-500 rounded-full">
-      {value}
-    </div>
-  );
+  if (value)
+    return (
+      <div className="absolute top-2 end-2 -translate-y-full translate-x-full flex justify-center items-center w-4 h-4 text-xs text-primary-light bg-red-500 rounded-full">
+        {value}
+      </div>
+    );
 }
 export default IconBadge;
