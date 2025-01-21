f860b77bb8509cae878905ee959696d29f477cde
aehamdy <ae.hamdy91@gmail.com>
2025-01-21 16:50:37 +0200

    Feat: Handle Wishlist page when empty by displaying EmptyWishlist

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
 import SectionHeader from "../components/SectionHeader";
 import { useWishlist } from "../context/wishlistContext";
+import EmptyWishlist from "../components/EmptyWishlist";
 
 function Wishlist() {
   const { wishlistProducts, removeFromWishlist } = useWishlist();
@@ -12,15 +14,19 @@ function Wishlist() {
         desc="Explore the items you've marked as favorites, ready to be added to your cart and enjoyed."
       />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-14 gap-x-6">
-        {wishlistProducts.map((product, index) => (
-          <ProductCard
-            key={index}
-            product={product}
-            removeFromWishlist={removeFromWishlist}
-          />
-        ))}
-      </div>
+      {wishlistProducts.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-y-14 gap-x-6">
+          {wishlistProducts.map((product, index) => (
+            <ProductCard
+              key={index}
+              product={product}
+              removeFromWishlist={removeFromWishlist}
+            />
+          ))}
+        </div>
+      ) : (
+        <EmptyWishlist />
+      )}
     </section>
   );
 }
