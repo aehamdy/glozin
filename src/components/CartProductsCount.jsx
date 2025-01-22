b441aefacf041238c2aea7f16fbee8e26876fd40
aehamdy <ae.hamdy91@gmail.com>
2025-01-22 17:29:31 +0200

    Fix: Add fallback value of 0 for 'value' prop to avoid rendering undefined

--- src/components/CartProductsCount.jsx
+++ src/components/CartProductsCount.jsx
@@ -2,7 +2,7 @@
 function CartProductsCount({ value }) {
   return (
     <h4 className="font-semibold text-xl text-heading-dark">
-      Shopping Cart ({value})
+      Shopping Cart ({value ? value : 0})
     </h4>
   );
 }
