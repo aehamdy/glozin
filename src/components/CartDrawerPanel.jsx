ab964e15804d9cadf75af88ad09063af10cd9d9b
aehamdy <ae.hamdy91@gmail.com>
2025-01-22 20:14:12 +0200

    Feat: Add functionality to calculate subtotal based on cart list

--- src/components/CartDrawerPanel.jsx
+++ src/components/CartDrawerPanel.jsx
@@ -1,9 +1,18 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+
 function CartDrawerPanel({ cartList }) {
+  const [subtotal, setSubtotal] = useState(0);
+
+  useEffect(() => {
+    setSubtotal(cartList.reduce((total, curr) => total + curr.price, 0));
+  }, [cartList]);
+
   return (
-    <div className="py-3 px-4 text-secondary-dark bg-cloud-gray border-t">
+    <div className="py-3 px-4 text-secondary-dark bg-cloud-gray">
       <div className="flex justify-between font-semibold">
         <div>Subtotal:</div>
-        <div>$300.0</div>
+        <div>${subtotal.toFixed(2)}</div>
       </div>
     </div>
   );
