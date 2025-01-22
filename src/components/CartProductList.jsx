a0ddce9408388d5586dc6433cc649bada9af9fa3
aehamdy <ae.hamdy91@gmail.com>
2025-01-22 20:31:06 +0200

    Feat: Add vertical scroll to CartProductList

--- src/components/CartProductList.jsx
+++ src/components/CartProductList.jsx
@@ -3,7 +3,7 @@ import ProductInCart from "./ProductInCart";
 /* eslint-disable react/prop-types */
 function CartProductList({ cartList }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-[460px] overflow-y-auto">
       {cartList.map((product, index) => (
         <ProductInCart key={index} product={product} />
       ))}
