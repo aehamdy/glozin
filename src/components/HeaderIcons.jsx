7a9541f6ed4a9eacfb06645a4a05b5245d47e603
aehamdy <ae.hamdy91@gmail.com>
2025-01-24 16:31:12 +0200

    Refactor: Wrap icons in HeaderIcons with Button component for better reusability

--- src/components/HeaderIcons.jsx
+++ src/components/HeaderIcons.jsx
@@ -3,33 +3,35 @@ import { Link } from "react-router-dom";
 import Icon from "./Icon";
 import { useWishlist } from "../context/wishlistContext";
 import IconBadge from "./IconBadge";
+import Button from "./Button";
 
 function HeaderIcons({ handleOpenCart, handleSearchOpening }) {
   const { wishlistProducts } = useWishlist();
 
   return (
     <div className="flex gap-4">
-      <Icon
-        name="search"
-        className="text-primary-dark hover:text-red-500"
-        onClickFunction={handleSearchOpening}
-      />
-      <Icon
-        name="user"
-        className="hidden lg:block text-primary-dark  hover:text-red-500"
-      />
-      <Link to="/wishlist" className="relative">
-        <IconBadge value={wishlistProducts.length} />
+      <Button handleClick={handleSearchOpening}>
+        <Icon name="search" className="text-primary-dark hover:text-red-500" />
+      </Button>
+      <Button className="hidden lg:block">
+        <Icon name="user" className="text-primary-dark  hover:text-red-500" />
+      </Button>
+      <Button>
+        <Link to="/wishlist" className="relative">
+          <IconBadge value={wishlistProducts.length} />
+          <Icon
+            name="wishlist"
+            className="hidden lg:block text-primary-dark  hover:text-red-500"
+          />
+        </Link>
+      </Button>
+      <Button>
         <Icon
-          name="wishlist"
-          className="hidden lg:block text-primary-dark  hover:text-red-500"
+          name="cart"
+          className="text-primary-dark hover:text-red-500"
+          onClickFunction={handleOpenCart}
         />
-      </Link>
-      <Icon
-        name="cart"
-        className="text-primary-dark hover:text-red-500"
-        onClickFunction={handleOpenCart}
-      />
+      </Button>
     </div>
   );
 }
