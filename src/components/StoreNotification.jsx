34295348d1770043cbcd3c6b8cc6726029ea5228
aehamdy <ae.hamdy91@gmail.com>
2025-01-24 21:57:58 +0200

    Feat: Adjust StoreNotification to handle the notification type style based on alert prop

--- src/components/StoreNotification.jsx
+++ src/components/StoreNotification.jsx
@@ -1,5 +1,16 @@
 /* eslint-disable react/prop-types */
-function StoreNotification({ message }) {
-  return <div className="py-1.5 px-2.5 border">{message}</div>;
+function StoreNotification({ alert, message }) {
+  let messageStyle;
+  if (alert.toLowerCase() === "error") {
+    messageStyle = "text-red-800 bg-red-50 border-red-300";
+  } else if (alert.toLowerCase() === "success") {
+    messageStyle = "text-green-700 bg-green-50 border-green-300";
+  }
+
+  return (
+    <div className={`py-1.5 px-3.5 ${messageStyle} border rounded-md`}>
+      {message}
+    </div>
+  );
 }
 export default StoreNotification;
