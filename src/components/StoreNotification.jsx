b326d4d299a40db36f9731db8d6c54e12d93865b
aehamdy <ae.hamdy91@gmail.com>
2025-01-24 21:18:49 +0200

    Feat: Create StoreNotification component

---
+++ src/components/StoreNotification.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable react/prop-types */
+function StoreNotification({ message }) {
+  return <div className="py-1.5 px-2.5 border">{message}</div>;
+}
+export default StoreNotification;
