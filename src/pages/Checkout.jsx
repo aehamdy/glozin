52c1d5e27b4fd8843af8a4629a5d5ee75287ab61
aehamdy <ae.hamdy91@gmail.com>
2025-01-27 04:30:03 +0200

    Feat: Add email input and delivery methods radio buttons in Checkout page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,19 @@
+import Icon from "../components/Icon";
 import StoreNotification from "../components/StoreNotification";
 
 function Checkout() {
+  const deliveryMethods = [
+    {
+      method: "Ship",
+      icon: "wishlist",
+      inputId: "delivery-ship",
+    },
+    {
+      method: "Pick in store",
+      icon: "filledStar",
+      inputId: "delivery-instore",
+    },
+  ];
   return (
     <section className="grid grid-cols-1 lg:grid-cols-12 px-horizontal-spacing text-black">
       <div className="checkout-form col-span-6">
@@ -10,46 +23,52 @@ function Checkout() {
             message="This store can't accept payments right now. Refresh this page
             or come back later."
           />
-          <form action="" className="flex flex-col items-start">
-            <div>
+          <form action="" className="flex flex-col items-start gap-4 w-full">
+            <div className="flex flex-col items-start gap-2 w-full">
               <h2 className="font-semibold text-xl text-black">Contact</h2>
+
+              <input
+                type="email"
+                name=""
+                id=""
+                placeholder="Type your email"
+                className="w-full py-1.5 px-2 bg-white border focus:border-blue-400 outline-none rounded-lg transition-all duration-short"
+              />
             </div>
 
-            <input
-              type="email"
-              name=""
-              id=""
-              placeholder="Type your email"
-              className="w-full py-1.5 px-2 bg-white border focus:outline-none rounded-md"
-            />
-
-            <div>
+            <div className="flex flex-col items-start gap-2 w-full">
               <h2 className="font-semibold text-xl text-black">Delivery</h2>
+
+              <ul className="w-full">
+                {deliveryMethods.map((method, index) => (
+                  <li key={index} className="w-full">
+                    <input
+                      type="radio"
+                      name="delivery"
+                      id={method.inputId}
+                      className="hidden peer"
+                    />
+                    <label
+                      htmlFor={method.inputId}
+                      className={`flex justify-between items-center w-full py-2 px-3 border ${
+                        index !== 0 && "border-t-0 peer-checked:border-t"
+                      }
+                    peer-checked:text-blue-500 peer-checked:bg-blue-50 peer-checked:border-blue-400 
+                    ${
+                      index === 0
+                        ? "rounded-t-lg peer-checked:rounded-t-lg"
+                        : index === deliveryMethods.length - 1 &&
+                          "rounded-b-lg peer-checked:rounded-b-lg"
+                    }
+                    cursor-pointer transition-all duration-200`}
+                    >
+                      <div className="text-secondary-dark">{method.method}</div>
+                      <Icon name={method.icon} size="18" />
+                    </label>
+                  </li>
+                ))}
+              </ul>
             </div>
-
-            <ul>
-              <li>
-                <input type="radio" name="" id="" />
-                <label htmlFor="" className="flex justify-between">
-                  <div>Ship</div>
-                  {/* <Icon name="wishlist" /> */}
-                </label>
-              </li>
-              <li>
-                <input type="radio" name="" id="" />
-                <label htmlFor="" className="flex justify-between">
-                  <div>Pickup in store</div>
-                  {/* <Icon name="wishlist" /> */}
-                </label>
-              </li>
-            </ul>
-
-            <select name="" id="">
-              <option value="CH">China</option>
-              <option value="FR">France</option>
-              <option value="UK">United Kingdom</option>
-              <option value="US">United States</option>
-            </select>
           </form>
         </div>
       </div>
