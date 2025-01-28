0da79d43031212aeca10328e953e3e63535cca8a
aehamdy <ae.hamdy91@gmail.com>
2025-01-28 23:07:22 +0200

    Feat: Add onChangeHandler to TextInput for state updates

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -7,9 +7,16 @@ function TextInput({
   placeholder,
   className,
   required,
+  setContact,
+  value,
 }) {
   const defaultStyles =
     "py-2 px-3 bg-white border focus:border-blue-400 outline-none rounded-lg transition-all duration-short";
+
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setContact((prev) => ({ ...(prev || {}), [name]: value }));
+  };
 
   return (
     <input
@@ -18,8 +25,11 @@ function TextInput({
       id={id}
       placeholder={placeholder}
       className={`${defaultStyles} ${className}`}
+      value={value}
+      onChange={onChangeHandler}
       required={required && required}
     />
   );
 }
+
 export default TextInput;
