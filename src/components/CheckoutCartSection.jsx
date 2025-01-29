d922b6a176c1819c7da7ae19a03dcf8a6de2c168
aehamdy <ae.hamdy91@gmail.com>
2025-01-29 22:39:10 +0200

    Feat: Create CheckoutCartSection component

---
+++ src/components/CheckoutCartSection.jsx
@@ -0,0 +1,6 @@
+function CheckoutCartSection() {
+  return (
+    <div className="col-span-6 py-8 lg:px-8 text-black bg-cloud-gray"></div>
+  );
+}
+export default CheckoutCartSection;
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,10 +1,11 @@
+import CheckoutCartSection from "../components/CheckoutCartSection";
 import CheckoutFormSection from "../components/CheckoutFormSection";
 
 function Checkout() {
   return (
     <section className="grid grid-cols-1 lg:grid-cols-12 px-horizontal-spacing text-black">
       <CheckoutFormSection />
-      <div className="checkout-cart-section col-span-6"></div>
+      <CheckoutCartSection />
     </section>
   );
 }
