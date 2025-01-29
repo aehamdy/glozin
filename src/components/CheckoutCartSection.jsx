503d403c6954cf04f8f3e5e170f5fbbc7856702e
aehamdy <ae.hamdy91@gmail.com>
2025-01-29 23:10:33 +0200

    Feat: Display cart products in CheckoutCartSection

--- src/components/CheckoutCartSection.jsx
+++ src/components/CheckoutCartSection.jsx
@@ -1,6 +1,33 @@
+import { useCart } from "../context/CartContext";
+
 function CheckoutCartSection() {
+  const { cartList } = useCart();
+
   return (
-    <div className="col-span-6 py-8 lg:px-8 text-black bg-cloud-gray"></div>
+    <div className="col-span-6 py-8 lg:px-8 text-[15px] text-black bg-cloud-gray">
+      <div className="flex flex-col gap-3">
+        {cartList.map((product, index) => (
+          <article key={index} className="flex justify-between gap-3">
+            <div className="flex gap-3">
+              <div className="relative w-[70px] border border-gray-300 rounded-md">
+                <img
+                  src={`${product.images[0]}`}
+                  alt={`${product.title} image`}
+                />
+              </div>
+
+              <div>
+                <span>{product.title}</span>
+              </div>
+            </div>
+
+            <div>
+              <span>${product.price}</span>
+            </div>
+          </article>
+        ))}
+      </div>
+    </div>
   );
 }
 export default CheckoutCartSection;
