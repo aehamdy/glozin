53428a043db294060a08e8559aed25a4f3d2124e
aehamdy <ae.hamdy91@gmail.com>
2025-02-02 00:31:36 +0200

    Style: Adjust SaleBanner height for consistency between all cards

--- src/components/SaleBanner.jsx
+++ src/components/SaleBanner.jsx
@@ -2,7 +2,7 @@
 function SaleBanner({ code }) {
   return (
     <article className="flex flex-col gap-[5px] md:gap-[2px]">
-      <div className="flex flex-col items-center gap-2 pt-8 pb-5 md:pt-16 md:pb-8 px-6 text-primary-light bg-[#fff3] border border-[#fff3] rounded-small backdrop-filter">
+      <div className="flex flex-col items-center gap-2 h-[170px] md:h-[200px] pt-8 pb-5 md:pt-16 md:pb-8 px-6 text-primary-light bg-[#fff3] border border-[#fff3] rounded-small backdrop-filter">
         <span className="w-fit py-2 px-5 font-semibold uppercase text-xs text-secondary-dark bg-primary-light rounded-medium">
           {code.label}
         </span>
--- src/components/SaleBannersWrapper.jsx
+++ src/components/SaleBannersWrapper.jsx
@@ -2,13 +2,14 @@ import discountCodes from "../data/discountCodes";
 import SaleBanner from "./SaleBanner";
 
 function SaleBannersWrapper() {
+  const activeCodes = discountCodes.filter(
+    (code) => code.active && code.viewOnApp
+  );
   return (
     <div className="w-full grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 md:gap-8 py-5 px-4 md:px-10">
-      {discountCodes
-        .filter((code) => code.active && code.viewOnApp)
-        .map((code, index) => (
-          <SaleBanner key={index} code={code} />
-        ))}
+      {activeCodes.map((code, index) => (
+        <SaleBanner key={index} code={code} />
+      ))}
     </div>
   );
 }
