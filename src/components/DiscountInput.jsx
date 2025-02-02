58f6ea8370e14f81dd9fbb2de9393218647de2de
aehamdy <ae.hamdy91@gmail.com>
2025-02-02 20:58:18 +0200

    Feat: Added useState for managing coupon code input

--- src/components/DiscountInput.jsx
+++ src/components/DiscountInput.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 function DiscountInput() {
-  return <input type="text" placeholder="Enter discount code" className="" />;
+  const [couponCode, setCouponCode] = useState("");
+
+  const onInputChange = (e) => {
+    setCouponCode(e.target.value.toUpperCase());
+  };
+
+  return (
+    <>
+      {console.log(couponCode)}
+      <input
+        type="text"
+        placeholder="Enter discount code"
+        onChange={onInputChange}
+        value={couponCode}
+        className="w-9/12 md:w-1/2 lg:w-3/5 py-3 px-2 text-black placeholder:text-price-originalPrice bg-white border outline-none rounded-md"
+      />
+    </>
+  );
 }
 export default DiscountInput;
