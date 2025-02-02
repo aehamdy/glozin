fe465f1c74a9d7f199968ddd0253363f747e2938
aehamdy <ae.hamdy91@gmail.com>
2025-02-02 21:16:49 +0200

    Style: Adjust ApplyDiscountButton style

--- src/components/ApplyDiscountButton.jsx
+++ src/components/ApplyDiscountButton.jsx
@@ -1,4 +1,11 @@
 function ApplyDiscountButton() {
-  return <button className="">Apply</button>;
+  return (
+    <button
+      disabled
+      className="px-4 text-content-light-dark bg-neutral-200 rounded-md"
+    >
+      Apply
+    </button>
+  );
 }
 export default ApplyDiscountButton;
