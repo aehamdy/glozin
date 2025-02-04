68648a856f3fe92943b816b91eec21b413b01e2a
aehamdy <ae.hamdy91@gmail.com>
2025-02-04 20:35:47 +0200

    Fix: Resolve the issue of showing shipping fees as discount price upon select a country

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -8,12 +8,14 @@ function Total({ shippingFees }) {
 
   return (
     <div className="flex items-center gap-3">
-      {newTotal > 0 && (
+      {newTotal > 0 && newSubtotal > 0 && (
         <div className={`text-red-500`}>$ {newTotal.toFixed(2)}</div>
       )}
       <div
         className={`flex gap-2 ${
-          newTotal && "font-medium text-base text-secondary-dark line-through"
+          newTotal > 0 &&
+          newSubtotal &&
+          "font-medium text-base text-secondary-dark line-through"
         }`}
       >
         $ {total.toFixed(2)}
