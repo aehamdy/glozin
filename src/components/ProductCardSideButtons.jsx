8284a3a82a82916030e39c8afb706ecac69fd619
aehamdy <ae.hamdy91@gmail.com>
2025-02-05 17:45:21 +0200

    Refactor: Make ProductCardSideButtons moves horizontally on hover on large screen only

--- src/components/ProductCardSideButtons.jsx
+++ src/components/ProductCardSideButtons.jsx
@@ -9,7 +9,7 @@ function ProductCardSideButtons({ productId, removeFromWishlist }) {
   const quickButtons = [{ icon: "wishlist" }, { icon: "eye" }];
 
   return (
-    <div className="absolute flex flex-col gap-4 top-5 end-2 lg:end-0 lg:translate-x-full group-hover:-translate-x-2 lg:opacity-0 lg:group-hover:opacity-100 group-hover:z-50 transition-all duration-medium">
+    <div className="absolute flex flex-col gap-4 top-5 end-2 lg:end-0 lg:translate-x-full group-hover:lg:-translate-x-2 lg:opacity-0 lg:group-hover:opacity-100 group-hover:z-50 transition-all duration-medium">
       <div className="relative flex flex-col gap-3">
         {quickButtons.map((button, index) =>
           button.icon === "wishlist" ? (
