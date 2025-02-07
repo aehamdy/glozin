c1380c579fee02c0f184ab531c2e8cac9c39bfda
aehamdy <ae.hamdy91@gmail.com>
2025-02-07 17:30:27 +0200

    Refactor: Change name of AddToWishlistButton into a proper component name which is WishlistButton

---
+++ src/components/WishlistButton.jsx
@@ -0,0 +1,48 @@
+/* eslint-disable react/prop-types */
+import { useFetchSingleProduct } from "../hooks/useFetchSingleProduct";
+import { useWishlist } from "../context/wishlistContext";
+import { useEffect, useState } from "react";
+import ProductCardButton from "./ProductCardButton";
+
+function WishlistButton({ productId, variant, tooltipValue }) {
+  const { addToWishlist, removeFromWishlist, wishlistProducts } = useWishlist();
+  const { product } = useFetchSingleProduct(productId);
+  const [isInWishlist, setIsInWishlist] = useState(false);
+
+  useEffect(() => {
+    if (product) {
+      const inWishlist = wishlistProducts.some(
+        (product) => product.id === productId
+      );
+      setIsInWishlist(inWishlist);
+    }
+  }, [wishlistProducts, productId]);
+
+  useEffect(() => {
+    const inWishlist = wishlistProducts.some(
+      (product) => product.id === productId
+    );
+    setIsInWishlist(inWishlist);
+  }, []);
+
+  const handleWishlistToggle = () => {
+    if (product) {
+      if (isInWishlist) {
+        removeFromWishlist(productId);
+      } else {
+        addToWishlist(product);
+      }
+    }
+  };
+
+  return (
+    <ProductCardButton
+      icon="wishlist"
+      onClickFunc={handleWishlistToggle}
+      isInWishlist={isInWishlist}
+      variant={variant}
+      tooltipValue={tooltipValue && tooltipValue}
+    />
+  );
+}
+export default WishlistButton;
