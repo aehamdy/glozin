1c6f7c911d5edcb135c734a9ae0ea8950c06b9db
aehamdy <ae.hamdy91@gmail.com>
2025-02-09 19:46:48 +0200

    Fix: Resolve the issue of displaying buy now item with cartList items upon click on checkout button in CartDrawerPanel

--- src/components/CartDrawerPanel.jsx
+++ src/components/CartDrawerPanel.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 import Subtotal from "./Subtotal";
+import { useCart } from "../context/CartContext";
 
 function CartDrawerPanel() {
+  const { setBuyNowProduct } = useCart();
+  const handleClick = () => {
+    setBuyNowProduct(null);
+  };
   return (
     <div className="flex flex-col justify-between gap-6 mt-auto pt-4 pb-4 px-4 text-secondary-dark bg-cloud-gray border-t">
       <div className="flex justify-between font-semibold">
@@ -13,6 +18,7 @@ function CartDrawerPanel() {
       <div className="flex flex-col items-center gap-5">
         <Link
           to="/checkout"
+          onClick={handleClick}
           className=" w-3/4 py-3 font-semibold text-primary-light bg-secondary-dark hover:bg-primary-dark rounded-medium"
         >
           Checkout
