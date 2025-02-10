cfa342fafd4de160a1a666513e26b654933ff33e
aehamdy <ae.hamdy91@gmail.com>
2025-02-10 14:13:59 +0200

    Refactor: Replaced <a> tag with <Link> for better client-side navigation

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,13 @@
-import logoImg from "../assets/logo.svg"
+import logoImg from "../assets/logo.svg";
 
 function Logo() {
-    return (
-        <div>
-            <a href="#">
-            <img src={logoImg} alt="logo image" />
-            </a>
-        </div>
-    )
+  return (
+    <div>
+      <a href="#">
+        <img src={logoImg} alt="logo image" />
+      </a>
+    </div>
+  );
 }
 
 export default Logo;
