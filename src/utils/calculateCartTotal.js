433c2b7aa20b10f9f64aad8ff60c268eb7234144
aehamdy <ae.hamdy91@gmail.com>
2025-02-13 21:05:01 +0200

    Fix: Correct typo in function parameter (cartLitst → cartList) in calculateCartTotal

--- src/utils/calculateCartTotal.js
+++ src/utils/calculateCartTotal.js
@@ -1,5 +1,5 @@
-const calculateCartTotal = (cartLitst) => {
-    return cartLitst.reduce((acc, curr) => acc + curr.price * curr.orderQuantity, 0);
+const calculateCartTotal = (cartList) => {
+    return cartList.reduce((acc, curr) => acc + curr.price * curr.orderQuantity, 0);
 };
 
 export default calculateCartTotal;
