e8f8b408954f9a804e6d842c90161bfc5ccbdb6f
aehamdy <ae.hamdy91@gmail.com>
2025-02-14 17:12:17 +0200

    Feat: Create constants/actionTypes.js to centralize reducer action types

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,4 @@
+export const ADD_TO_CART = "addToCart";
+export const REMOVE_FROM_CART = "removeFromCart";
+export const INCREASE_QUANTITY = "increaseQuantity";
+export const DECREASE_QUANTITY = "decreaseQuantity";
