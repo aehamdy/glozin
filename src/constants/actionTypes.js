a835fb7121ff1b7f4bb75aa762892d46e3166a89
aehamdy <ae.hamdy91@gmail.com>
2025-02-14 22:00:37 +0200

    Feat: Add Checkout action types to actionTypes.js

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,4 +1,11 @@
+// Cart Action Types
 export const ADD_TO_CART = "addToCart";
 export const REMOVE_FROM_CART = "removeFromCart";
 export const INCREASE_QUANTITY = "increaseQuantity";
 export const DECREASE_QUANTITY = "decreaseQuantity";
+
+// Checkout Action Types
+export const SET_SUBTOTAL = "SET_SUBTOTAL";
+export const SET_DISCOUNTED_SUBTOTAL = "SET_DISCOUNTED_SUBTOTAL";
+export const SET_TOTAL = "SET_TOTAL";
+export const SET_DISCOUNTED_TOTAL = "SET_DISCOUNTED_TOTAL";
