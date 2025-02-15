4725ece3fd36bb2ff08d993ce845889fd2fd0ce3
aehamdy <ae.hamdy91@gmail.com>
2025-02-15 16:16:54 +0200

    Refactor: Add UserData action types in actionTypes.js

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -10,3 +10,16 @@ export const SET_DISCOUNTED_SUBTOTAL = "SET_DISCOUNTED_SUBTOTAL";
 export const SET_TOTAL = "SET_TOTAL";
 export const SET_DISCOUNTED_TOTAL = "SET_DISCOUNTED_TOTAL";
 export const SET_SHIPPING_FEES = "SET_SHIPPING_FEES";
+
+//UserData Action Types
+export const SET_EMAIL = "SET_EMAIL";
+export const SET_DELIVERY_METHOD = "SET_DELIVERY_METHOD";
+export const SET_COUNTRY = "SET_COUNTRY";
+export const SET_FIRST_NAME = "SET_FIRST_NAME";
+export const SET_LAST_NAME = "SET_LAST_NAME";
+export const SET_ADDRESS = "SET_ADDRESS";
+export const SET_APARTMENT = "SET_APARTMENT";
+export const SET_CITY = "SET_CITY";
+export const SET_ZIP_CODE = "SET_ZIP_CODE";
+export const SET_LOCATION_FEES = "SET_LOCATION_FEES";
+export const SET_IS_COUPON_USED = "SET_IS_COUPON_USED";
