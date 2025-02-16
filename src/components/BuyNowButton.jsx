ba26b5152e31a55dd9ead66fa32524456ff1b383
aehamdy <ae.hamdy91@gmail.com>
2025-02-16 22:55:23 +0200

    Refactor: Move button click handler inside

--- src/components/BuyNowButton.jsx
+++ src/components/BuyNowButton.jsx
@@ -1,10 +1,18 @@
 /* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import Button from "./Button";
+import { useCart } from "../context/CartContext";
+import { SET_BUY_NOW_PRODUCT } from "../constants/actionTypes";
 
-function BuyNowButton({ product, isAgreementChecked, handleBuyNowClick }) {
+function BuyNowButton({ product, isAgreementChecked }) {
+  const { dispatchCart } = useCart();
+
   const handleOnClick = (e) => {
-    handleBuyNowClick(e, product);
+    if (isAgreementChecked) {
+      dispatchCart({ type: SET_BUY_NOW_PRODUCT, payload: product });
+    } else {
+      e.preventDefault();
+    }
   };
 
   return (
@@ -12,7 +20,7 @@ function BuyNowButton({ product, isAgreementChecked, handleBuyNowClick }) {
       {isAgreementChecked ? (
         <Link
           to="/checkout"
-          onClick={(e) => handleOnClick(e, product)}
+          onClick={(e) => handleOnClick(e)}
           className={`py-2.5 text-primary-light bg-red-500 hover:bg-red-600 active:bg-red-700 rounded-medium duration-short`}
         >
           Buy Now
