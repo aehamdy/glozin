7aa55778c1e6814ee557d0f60d3c47897d0586bf
aehamdy <ae.hamdy91@gmail.com>
2025-02-16 16:36:36 +0200

    Refactor: Handle displaying total amount only when exist in Total

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,24 +1,22 @@
-import { useCart } from "../context/CartContext";
 import { useCheckout } from "../context/CheckoutContext";
 
 function Total() {
-  const { newSubtotal } = useCart();
-  const { shippingFees, total } = useCheckout();
-  const newTotal = newSubtotal + shippingFees;
+  const { total } = useCheckout();
 
   return (
     <div className="flex items-center gap-3">
-      {newTotal > 0 && newSubtotal > 0 && (
+      {/* {newTotal > 0 && newSubtotal > 0 && (
         <div className={`text-red-500`}>$ {newTotal.toFixed(2)}</div>
-      )}
+      )} */}
       <div
         className={`flex gap-2 ${
-          newTotal > 0 &&
-          newSubtotal &&
-          "font-medium text-base text-secondary-dark line-through"
+          // newTotal > 0 &&
+          // newSubtotal &&
+          // "font-medium text-base text-secondary-dark line-through"
+          ""
         }`}
       >
-        $ {total.toFixed(2)}
+        $ {total && total.toFixed(2)}
       </div>
     </div>
   );
