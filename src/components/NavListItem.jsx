6230870181f2e0a67e6824fd393cd495addabc2d
aehamdy <ae.hamdy91@gmail.com>
2025-02-17 21:01:31 +0200

    Refactor: Wrap li element inside Link for better accessibility and semantics

--- src/components/NavListItem.jsx
+++ src/components/NavListItem.jsx
@@ -4,21 +4,25 @@ import NavItemBadge from "./NavItemBadge";
 
 function NavListItem({ item }) {
   return (
-    <li
-      className={`relative before:absolute before:start-0 before:bottom-0 before:h-[2px] before:w-0 lg:hover:before:w-full 
-        before:bg-secondary-red flex justify-between w-full py-4 lg:py-2 lg:px-2 font-semibold 
+    <>
+      <Link
+        to={item.url}
+        className={`relative before:absolute before:start-0 before:bottom-0 before:h-[2px] before:w-0 
+          lg:hover:before:w-full before:bg-secondary-red flex justify-between w-full py-4 lg:py-2 lg:px-2 font-semibold 
         ${
           item.label.toLowerCase() === "sale" ||
           item.label.toLowerCase() === "clearance"
             ? "text-primary-red hover:text-secondary-red"
             : "text-secondary-dark hover:text-secondary-red"
-        } lg:border-none cursor-pointer duration-medium before:duration-medium`}
-    >
-      <Link to={item.url} className="flex relative">
-        {item.label.charAt(0).toUpperCase() + item.label.slice(1).toLowerCase()}
-        {item.badge && <NavItemBadge badgeText={item.badge} />}
+        } lg:border-none duration-medium before:duration-medium`}
+      >
+        <li className="flex relative">
+          {item.label.charAt(0).toUpperCase() +
+            item.label.slice(1).toLowerCase()}
+          {item.badge && <NavItemBadge badgeText={item.badge} />}
+        </li>
       </Link>
-    </li>
+    </>
   );
 }
 export default NavListItem;
