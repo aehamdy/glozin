b361aaeb8131f1464431138bdadef207b8cfa9d7
aehamdy <ae.hamdy91@gmail.com>
2025-02-18 17:45:07 +0200

    Refactor: Extract enteredCouponCode from checkout context to use it

--- src/components/DiscountInput.jsx
+++ src/components/DiscountInput.jsx
@@ -1,19 +1,19 @@
-import { SET_COUPON_CODE } from "../constants/actionTypes";
+import { SET_ENTERED_COUPON_CODE } from "../constants/actionTypes";
 import { useCheckout } from "../context/CheckoutContext";
 
 /* eslint-disable react/prop-types */
 function DiscountInput({ handleOnApplyClick }) {
-  const { dispatchCheckout, usedCouponCode, discountError } = useCheckout();
+  const { dispatchCheckout, enteredCouponCode, discountError } = useCheckout();
 
   const onInputChange = (e) => {
     dispatchCheckout({
-      type: SET_COUPON_CODE,
+      type: SET_ENTERED_COUPON_CODE,
       payload: e.target.value.toUpperCase(),
     });
   };
 
   const handleEnterKeyPress = (e) => {
-    if (usedCouponCode && e.key === "Enter") {
+    if (enteredCouponCode && e.key === "Enter") {
       handleOnApplyClick();
     }
   };
@@ -25,7 +25,7 @@ function DiscountInput({ handleOnApplyClick }) {
         placeholder="Enter discount code"
         onChange={onInputChange}
         onKeyUp={handleEnterKeyPress}
-        value={usedCouponCode}
+        value={enteredCouponCode}
         className={`w-8/12 md:w-1/2 lg:w-3/5 py-3 px-2 font-medium placeholder:font-normal text-black placeholder:text-price-originalPrice bg-white border-2 ${
           discountError && "border-red-400 focus:border-red-400"
         } focus:border-blue-500 outline-none rounded-md`}
