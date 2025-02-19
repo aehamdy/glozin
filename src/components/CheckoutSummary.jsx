877acfa06611fb7a4a7c00ceeb9e639c3b30f42d
aehamdy <ae.hamdy91@gmail.com>
2025-02-19 22:29:50 +0200

    Refactor: Refactor code structure and handle the state outside the function return

--- src/components/CheckoutSummary.jsx
+++ src/components/CheckoutSummary.jsx
@@ -20,8 +20,29 @@ function CheckoutSummary() {
     setItemsCount(cartList.reduce((acc, curr) => acc + curr.orderQuantity, 0));
   }, [cartList]);
 
+  const shippingMessage =
+    isEligibleForFreeShipping ||
+    (discountedShippingFees === 0 && "You’ve got free shipping");
+
+  const shippingCost =
+    // discountedShippingFees === 0 ||
+    // discountedShippingFees > 0 ||
+    // (isEligibleForFreeShipping && countryValue)
+    //   ? `$ ${discountedShippingFees}`
+    //   : isEligibleForFreeShipping && !countryValue
+    //   ? "Select country"
+    //   : shippingFees && countryValue
+    //   ? `$ ${shippingFees}`
+    //   : "Select country";
+    discountedShippingFees >= 0 && discountedShippingFees != null
+      ? `$ ${discountedShippingFees}`
+      : (countryValue && isEligibleForFreeShipping) || countryValue
+      ? `$ ${shippingFees}`
+      : "Select country";
+
   return (
     <div className="flex flex-col gap-3">
+      {/* Subtotal Section */}
       <div className="flex justify-between items-center">
         <div className="flex items-center gap-2">
           <span>Subtotal</span>
@@ -33,35 +54,26 @@ function CheckoutSummary() {
         />
       </div>
 
+      {/* Shipping Section */}
       <div className="flex justify-between">
         <span>Shipping</span>
-
         <div className="flex items-center gap-5">
-          {isEligibleForFreeShipping ||
-            (discountedShippingFees === 0 && (
-              <span className="font-semibold text-xs text-green-500">
-                You&apos;ve got free shipping
-              </span>
-            ))}
-
+          {shippingMessage && (
+            <span className="font-semibold text-xs text-green-500">
+              {shippingMessage}
+            </span>
+          )}
           <span
             className={`font-normal text-sm ${
               shippingFees ? "text-black" : "text-price-originalPrice"
             }`}
           >
-            {discountedShippingFees === 0 || discountedShippingFees > 0
-              ? `$ ${discountedShippingFees}`
-              : isEligibleForFreeShipping && countryValue
-              ? `$ ${discountedShippingFees}`
-              : isEligibleForFreeShipping && !countryValue
-              ? "Select country"
-              : shippingFees && countryValue
-              ? `$ ${shippingFees}`
-              : "Select country"}
+            {shippingCost}
           </span>
         </div>
       </div>
 
+      {/* Total Section */}
       <div className="flex justify-between font-semibold text-lg">
         <span>Total</span>
         <Total />
@@ -69,4 +81,5 @@ function CheckoutSummary() {
     </div>
   );
 }
+
 export default CheckoutSummary;
