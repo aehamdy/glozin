88885d69878e06583038569c83c914630c7283b9
aehamdy <ae.hamdy91@gmail.com>
2025-02-19 14:58:07 +0200

    Refactor: Add type field which declares whether the discount is percentage, fixed or free shipping

--- src/data/discountCodes.js
+++ src/data/discountCodes.js
@@ -2,6 +2,7 @@ const discountCodes = [
     {
         label: "holiday",
         discountAmount: 30,
+        type: 'percentage',
         title: `Sale 30% Off`,
         desc: `30% off during the holiday season.`,
         code: `holiday30all`,
@@ -13,6 +14,7 @@ const discountCodes = [
     {
         label: "bundle",
         discountAmount: 50,
+        type: 'percentage',
         title: `Sale 50% Off`,
         desc: `Discount when buying combo.`,
         code: `bundledeal50`,
@@ -24,6 +26,7 @@ const discountCodes = [
     {
         label: "flashsale",
         discountAmount: 75,
+        type: 'percentage',
         title: `Sale 75% Off`,
         desc: `Big discounts for Flash Sale hours.`,
         code: `flashsale75`,
@@ -35,6 +38,7 @@ const discountCodes = [
     {
         label: "white friday",
         discountAmount: 50,
+        type: 'percentage',
         title: `Best Of Sale`,
         desc: `Extra 50% off for VIP customers.`,
         code: `vipsale50all`,
@@ -46,6 +50,7 @@ const discountCodes = [
     {
         label: "free shipping",
         discountAmount: 0,
+        type: 'free_shipping',
         title: `Free Shipping`,
         desc: `Enjoy free shipping for your orders`,
         code: `freship`,
