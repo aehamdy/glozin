6f6050d4b4f70700faa88869c3ad6f014e3d8c0f
aehamdy <ae.hamdy91@gmail.com>
2025-02-19 22:30:29 +0200

    Remove toFixed method

--- src/utils/calculateCartTotal.js
+++ src/utils/calculateCartTotal.js
@@ -1,6 +1,6 @@
 const calculateCartTotal = (cartList) => {
     const totalPrice = Array.isArray(cartList) ? cartList.reduce((acc, curr) => acc + curr.price * curr.orderQuantity, 0) : cartList.price;
-    return totalPrice.toFixed(2);
+    return totalPrice;
 };
 
 export default calculateCartTotal;
