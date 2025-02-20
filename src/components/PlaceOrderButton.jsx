0a6e98298deb77b16c5faeae311dc17a18190346
aehamdy <ae.hamdy91@gmail.com>
2025-02-20 20:04:24 +0200

    Feat: Handle button status according to user data validity

--- src/components/PlaceOrderButton.jsx
+++ src/components/PlaceOrderButton.jsx
@@ -1,20 +1,26 @@
+import { Link } from "react-router-dom";
 import { useUserData } from "../context/UserDataContext";
 
 function PlaceOrderButton() {
   const { isUserDataValid } = useUserData();
   const isValid = isUserDataValid;
+  const buttonValue = "Place Order";
+  const defaultStyles = "w-full mt-4 py-3 font-semibold text-lg";
 
-  return (
-    <input
-      type="submit"
-      value="Pay now"
-      className={`w-full mt-4 py-3 font-semibold text-lg  ${
-        isValid
-          ? "text-white bg-blue-500 hover:bg-blue-600 active:bg-blue-700 shadow-lg active:shadow-none transition-all duration-short cursor-pointer"
-          : "text-[#707070] bg-cloud-gray cursor-not-allowed"
-      }`}
-      disabled={isValid}
-    />
+  return isValid ? (
+    <Link
+      to=""
+      className={`${defaultStyles} text-white bg-blue-500 hover:bg-blue-600 active:bg-blue-700 shadow-md hover:shadow-xl active:shadow-none transition-all duration-short cursor-pointer`}
+    >
+      {buttonValue}
+    </Link>
+  ) : (
+    <button
+      className={`${defaultStyles} text-[#707070] bg-cloud-gray cursor-not-allowed`}
+      disabled
+    >
+      {buttonValue}
+    </button>
   );
 }
 export default PlaceOrderButton;
