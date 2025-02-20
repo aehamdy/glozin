c36f0446e61a250497a3570c1ca32139d949989d
aehamdy <ae.hamdy91@gmail.com>
2025-02-20 19:36:10 +0200

    Style: Add different styles to the button depending on its status

--- src/components/PlaceOrderButton.jsx
+++ src/components/PlaceOrderButton.jsx
@@ -8,7 +8,11 @@ function PlaceOrderButton() {
     <input
       type="submit"
       value="Pay now"
-      className="w-full mt-4 py-3 font-semibold text-lg"
+      className={`w-full mt-4 py-3 font-semibold text-lg  ${
+        isValid
+          ? "text-white bg-blue-500 hover:bg-blue-600 active:bg-blue-700 shadow-lg active:shadow-none transition-all duration-short cursor-pointer"
+          : "text-[#707070] bg-cloud-gray cursor-not-allowed"
+      }`}
       disabled={isValid}
     />
   );
