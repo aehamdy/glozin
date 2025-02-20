c3d33d44260581564b4e18ed7fd25f22a9c9f4d3
aehamdy <ae.hamdy91@gmail.com>
2025-02-20 21:03:15 +0200

    Create routes file

---
+++ src/config/routes.js
@@ -0,0 +1,16 @@
+const ROUTES = {
+    HOME: "/",
+    SHOP: "/shop",
+    PRODUCT_DETAILS: "/product/:id",
+    COLLECTIONS: "/collections",
+    SALE: "/sale",
+    WISHLIST: "/wishlist",
+    ABOUT: "/about",
+    CONTACT: "/contact",
+    OURSTORES: "/ourstores",
+    HELP: "/help",
+    CHECKOUT: "/checkout",
+    ORDER_CONFIRMATION: "/order-confirmation",
+}
+
+export default ROUTES;
