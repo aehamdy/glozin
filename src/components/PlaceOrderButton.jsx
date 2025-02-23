5dfea719ae8510eef17bd57727b1ba8ece862776
aehamdy <ae.hamdy91@gmail.com>
2025-02-23 16:32:52 +0200

    Feat: Create setOrder function and execute it upon click on place order button

--- src/components/PlaceOrderButton.jsx
+++ src/components/PlaceOrderButton.jsx
@@ -3,17 +3,31 @@ import { useUserData } from "../context/UserDataContext";
 import getNowDate from "../utils/getNowDate";
 import ROUTES from "../config/routes";
 import { useCheckout } from "../context/CheckoutContext";
-import { SET_ORDER_DATE } from "../constants/actionTypes";
+import { useCart } from "../context/CartContext";
+import { SET_ORDER } from "../constants/actionTypes";
 
 function PlaceOrderButton() {
   const { isUserDataValid } = useUserData();
-  const { dispatchCheckout } = useCheckout();
+  const { cartList, buyNowProduct } = useCart();
+  const { dispatchCheckout, subtotal, shippingFees, total } = useCheckout();
   const isValid = isUserDataValid;
   const buttonValue = "Place Order";
   const defaultStyles = "w-full mt-4 py-3 font-semibold text-lg";
 
+  const setOrder = () => {
+    if (buyNowProduct) {
+      dispatchCheckout({ type: SET_ORDER.items, payload: [buyNowProduct] });
+    } else {
+      dispatchCheckout({ type: SET_ORDER.items, payload: cartList });
+    }
+    dispatchCheckout({ type: SET_ORDER.subtotal, payload: subtotal });
+    dispatchCheckout({ type: SET_ORDER.shipping, payload: shippingFees });
+    dispatchCheckout({ type: SET_ORDER.total, payload: total });
+    dispatchCheckout({ type: SET_ORDER.date, payload: getNowDate() });
+  };
+
   const onClickHandler = () => {
-    dispatchCheckout({ type: SET_ORDER_DATE, payload: getNowDate() });
+    setOrder();
   };
 
   return isValid ? (
