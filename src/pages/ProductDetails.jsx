f8e8b3d84c2a55b0310bf29ecd2d3c84b45ddeba
aehamdy <ae.hamdy91@gmail.com>
2025-02-24 18:00:28 +0200

    Refactor: Replace old rating total stars and minimum stock limit by new ones

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,21 +3,12 @@ import GoBackButton from "../components/GoBackButton";
 import ProductImages from "../components/ProductImages";
 import ProductInfo from "../components/ProductInfo";
 import ProductReviews from "../components/ProductReviews";
+import { shopConfig } from "../config/shopConfig";
 
 function ProductDetails() {
   const { id } = useParams();
   const location = useLocation();
   const product = location.state;
-  const RATING_TOTAL_STARS = 5;
-  const MINIMUM_STOCK_LIMIT = 5;
-
-  //   if (!product) {
-  //     return (
-  //       <p className="text-red-500">
-  //         Product details not available. Please navigate from the product list.
-  //       </p>
-  //     );
-  //   }
 
   return (
     <section className="m-horizontal-spacing lg:p-horizontal-spacing">
@@ -26,14 +17,14 @@ function ProductDetails() {
         <ProductImages images={product.images} productTitle={product.title} />
         <ProductInfo
           product={product}
-          totalStars={RATING_TOTAL_STARS}
-          minimumStock={MINIMUM_STOCK_LIMIT}
+          totalStars={shopConfig.ratingTotalStars}
+          minimumStock={shopConfig.minimumStockLimit}
         />
       </div>
 
       <ProductReviews
         productReviews={product.reviews}
-        totalStars={RATING_TOTAL_STARS}
+        totalStars={shopConfig.ratingTotalStars}
       />
     </section>
   );
