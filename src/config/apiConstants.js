3ded0b235d4dd89ce2ba7d360164e68651101664
aehamdy <ae.hamdy91@gmail.com>
2025-02-25 18:18:00 +0200

    Create constants file with API base URL and categories list

---
+++ src/config/apiConstants.js
@@ -0,0 +1,12 @@
+export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
+
+export const categories = [
+    {name: "dresses", endPoint: "/products/category/womens-dresses"},
+    {name: "tops", endPoint: "/products/category/tops"},
+    {name: "bags", endPoint: "/products/category/womens-bags"},
+    {name: "jewellery", endPoint: "/products/category/womens-jewellery"},
+    {name: "shoes", endPoint: "/products/category/womens-shoes"},
+    {name: "fragrances", endPoint: "/products/category/fragrances"},
+    {name: "watches", endPoint: "/products/category/womens-watches"},
+    {name: "beauty", endPoint: "/products/category/beauty"},
+];
