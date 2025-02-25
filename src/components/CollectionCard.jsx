02faa2b8c1d414078e9c54862c48e38dd4e4c733
aehamdy <ae.hamdy91@gmail.com>
2025-02-25 19:13:47 +0200

    Feat: Set loading attribute to CollectionCard image to lazy for better performance

--- src/components/CollectionCard.jsx
+++ src/components/CollectionCard.jsx
@@ -8,6 +8,7 @@ function CollectionCard({ collection }) {
         <img
           src={collection.image}
           alt={`${collection.name}'s image`}
+          loading="lazy"
           className="w-full h-full top-0 start-0"
         />
         <div className="absolute w-8/12 start-1/2 bottom-4 lg:bottom-8 -translate-x-1/2 py-2.5 md:py-4 lg:py-3 px-3 md:px-6 lg:px-8 font-semibold text-primary-dark group-hover:text-primary-light bg-primary-light group-hover:bg-secondary-dark rounded-medium duration-medium">
