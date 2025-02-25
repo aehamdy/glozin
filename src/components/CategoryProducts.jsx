34c1ad7c5e259752dd240e0e5d59ad85a75ac031
aehamdy <ae.hamdy91@gmail.com>
2025-02-25 23:29:21 +0200

    Feat: Impelemnt usage of useFetchCategoryProducts hook and send product list as a prop to ProductWrapper

--- src/components/CategoryProducts.jsx
+++ src/components/CategoryProducts.jsx
@@ -1,4 +1,19 @@
+import ProductsWrapper from "./ProductsWrapper";
+import useFetchCategoryProducts from "../hooks/useFetchCategoryProducts";
+import { useParams } from "react-router-dom";
+
 function CategoryProducts() {
-  return <div>Category Products</div>;
+  const { categoryName } = useParams();
+  const [productList, isLoading, isError] =
+    useFetchCategoryProducts(categoryName);
+
+  if (isLoading) return <p className="text-black">Loading...</p>;
+  if (isError) return <p className="text-black">Error: {isError}</p>;
+
+  return (
+    <section className="text-black">
+      <ProductsWrapper products={productList} />
+    </section>
+  );
 }
 export default CategoryProducts;
