49b1ba08bbc2d0982ede10ca85a1242a74c51f98
aehamdy <ae.hamdy91@gmail.com>
2025-02-25 23:31:27 +0200

    Feat: Create a function that fetches products depending on selected category

---
+++ src/hooks/useFetchCategoryProducts.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { BASE_API_URL, productCategoryKey } from "../config/apiConstants";
+
+const useFetchCategoryProducts = (categoryName) => {
+    const [productList, setProductList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(null);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const url = `${BASE_API_URL}${productCategoryKey}/${categoryName}`
+                
+                const response = await fetch(url);
+
+                if (!response.ok) {
+                    throw new Error('Faild to fetch products');
+                }
+
+                const data = await response.json()
+
+                setProductList(data.products);
+
+            } catch(error) {
+
+                setIsError(error.message);
+
+            } finally {
+
+                setIsLoading(false);
+
+            }
+        };
+
+        fetchProducts()
+    }, [categoryName]);
+
+    return [productList, isLoading, isError]
+};
+
+export default useFetchCategoryProducts;
