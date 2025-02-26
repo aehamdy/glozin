8a07d057a987b5b9a746eb61da156eff68729b73
aehamdy <ae.hamdy91@gmail.com>
2025-02-26 17:41:00 +0200

    Refactor: Improve CategoriesWrapper maintainability by mapping over categories once

--- src/components/CategoriesWrapper.jsx
+++ src/components/CategoriesWrapper.jsx
@@ -3,17 +3,13 @@ import categories from "../data/categories";
 import CategoryCard from "./CategoryCard";
 
 function CategoriesWrapper({ variant }) {
+  const displayedCategories = variant ? categories.slice(0, 6) : categories;
+
   return (
     <ul className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-10 md:gap-5 lg:gap-8 px-horizontal-spacing">
-      {variant
-        ? categories
-            .slice(0, 6)
-            .map((category, index) => (
-              <CategoryCard key={index} category={category} variant={variant} />
-            ))
-        : categories.map((category, index) => (
-            <CategoryCard key={index} category={category} variant={variant} />
-          ))}
+      {displayedCategories.map((category, index) => (
+        <CategoryCard key={index} category={category} variant={variant} />
+      ))}
     </ul>
   );
 }
