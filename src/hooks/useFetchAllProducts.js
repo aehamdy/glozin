faa2c5995dbbe541c9446e1afd5dd0a8255e8ed1
aehamdy <ae.hamdy91@gmail.com>
2025-02-26 23:35:14 +0200

    Refactor: Implement usage of endPoints for consistency

--- src/hooks/useFetchAllProducts.js
+++ src/hooks/useFetchAllProducts.js
@@ -1,50 +1,48 @@
 import { useEffect, useState } from "react";
-import { BASE_API_URL, productCategoryKey } from "../config/apiConstants";
+import { BASE_API_URL, endPoints, productCategoryKey } from "../config/apiConstants";
 
-function useFetchAllProducts () {
-    const [productList, setProductList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+function useFetchAllProducts() {
+  const [productList, setProductList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchAllProducts = async () => {
-            try {
-                const categories = ['smartphones', 'womens-dresses'];
+  useEffect(() => {
+    const fetchAllProducts = async () => {
+      try {
+        const url = `${BASE_API_URL}${productCategoryKey}`;
 
-                const responses = await Promise.all(categories.map(
-                    category => 
-                        fetch(`${BASE_API_URL}${productCategoryKey}/${category}`)
-                    )
-                );
+        const responses = await Promise.all(
+          Object.values(endPoints).map((category) =>
+            fetch(`${url}/${category}`)
+          )
+        );
 
-                for (const response of responses) {
-                    if (!response.ok) {
-                        throw new Error ('Something went wrong while fetching products.');
-                    }
-                }
-
-                const data = await Promise.all(responses.map(response => response.json()));
-
-                const allProducts = data.flatMap(categoryData => categoryData.products);
-
-                setProductList(allProducts);
-
-            } catch (error) {
-
-                setError(error.message);
-
-            } finally {
-
-                setIsLoading(false);
-
-            }
+        for (const response of responses) {
+          if (!response.ok) {
+            throw new Error("Something went wrong while fetching products.");
+          }
         }
 
-        fetchAllProducts();
+        const data = await Promise.all(
+          responses.map((response) => response.json())
+        );
 
-    }, []);
+        const allProducts = data.flatMap(
+          (categoryData) => categoryData.products
+        );
 
-    return [productList, isLoading, error];
-};
+        setProductList(allProducts);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchAllProducts();
+  }, []);
+
+  return [productList, isLoading, error];
+}
 
 export default useFetchAllProducts;
