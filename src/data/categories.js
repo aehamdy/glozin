987060056932921a618efd3aa939d077cbee8a16
aehamdy <ae.hamdy91@gmail.com>
2025-02-26 19:28:56 +0200

    Replace images paths with new paths

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,21 +1,22 @@
-import menCollection from "../assets/category-men.webp";
-import womenCollection from "../assets/category-women.webp";
-import womenDividedCollection from "../assets/category-women-divided.webp";
-import sportsCollection from "../assets/category-sports.webp";
-import kidsCollection from "../assets/category-kids.webp";
-import babyCollection from "../assets/category-baby.webp";
-import homeCollection from "../assets/category-home.webp";
+import dressesCategory from "../assets/categories/dresses-category.webp";
+import topsCategory from "../assets/categories/tops-category.webp";
+import bagsCategory from "../assets/categories/bags-category.webp";
+import jewelleryCateogry from "../assets/categories/jewellery-category.webp";
+import shoesCategory from "../assets/categories/shoes-category.webp";
+import fragrancesCategory from "../assets/categories/fragrances-category.webp";
+import watchesCategory from "../assets/categories/watches-category.webp";
+import beautyCategory from "../assets/categories/beauty-category.webp";
 
 
 const categories = [
-    {name: "dresses", image: menCollection, endPoint: `/womens-dresses`},
-    {name: "tops", image: menCollection, endPoint: `/tops`},
-    {name: "bags", image: womenCollection, endPoint: `/womens-bags`},
-    {name: "jewellery", image: womenDividedCollection, endPoint: `/womens-jewellery`},
-    {name: "shoes", image: sportsCollection, endPoint: `/womens-shoes`},
-    {name: "fragrances", image: kidsCollection, endPoint: `/fragrances`},
-    {name: "watches", image: babyCollection, endPoint: `/womens-watches`},
-    {name: "beauty", image: homeCollection, endPoint: `/beauty`},
+    {name: "dresses", image: dressesCategory, endPoint: `/womens-dresses`},
+    {name: "tops", image: topsCategory, endPoint: `/tops`},
+    {name: "bags", image: bagsCategory, endPoint: `/womens-bags`},
+    {name: "jewellery", image: jewelleryCateogry, endPoint: `/womens-jewellery`},
+    {name: "shoes", image: shoesCategory, endPoint: `/womens-shoes`},
+    {name: "fragrances", image: fragrancesCategory, endPoint: `/fragrances`},
+    {name: "watches", image: watchesCategory, endPoint: `/womens-watches`},
+    {name: "beauty", image: beautyCategory, endPoint: `/beauty`},
 ];
 
 export default categories;
