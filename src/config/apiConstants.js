cb5bf7f4b82f6971862bdc4a96cabe85a53d13e0
aehamdy <ae.hamdy91@gmail.com>
2025-02-26 23:40:25 +0200

    Fix: Add '/' before each value of endPoints object

--- src/config/apiConstants.js
+++ src/config/apiConstants.js
@@ -3,12 +3,12 @@ export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 export const productCategoryKey = "/products/category";
 
 export const endPoints = {
-  womenBeauty: "beauty",
-  womenDresses: "womens-dresses",
-  womenFragrances: "fragrances",
-  womenTops: "tops",
-  womenBags: "womens-bags",
-  womenJewellery: "womens-jewellery",
-  womenShoes: "womens-shoes",
-  womenWatches: "womens-watches",
+  womenBeauty: "/beauty",
+  womenDresses: "/womens-dresses",
+  womenFragrances: "/fragrances",
+  womenTops: "/tops",
+  womenBags: "/womens-bags",
+  womenJewellery: "/womens-jewellery",
+  womenShoes: "/womens-shoes",
+  womenWatches: "/womens-watches",
 };
