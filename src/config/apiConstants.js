6802431ddc742ae8720dd7b21bf5c8c594f96fbb
aehamdy <ae.hamdy91@gmail.com>
2025-02-26 23:34:14 +0200

    Feat: Create endPoints object that contains all api endpoints for consistency

--- src/config/apiConstants.js
+++ src/config/apiConstants.js
@@ -1,3 +1,14 @@
 export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 
 export const productCategoryKey = "/products/category";
+
+export const endPoints = {
+  womenBeauty: "beauty",
+  womenDresses: "womens-dresses",
+  womenFragrances: "fragrances",
+  womenTops: "tops",
+  womenBags: "womens-bags",
+  womenJewellery: "womens-jewellery",
+  womenShoes: "womens-shoes",
+  womenWatches: "womens-watches",
+};
