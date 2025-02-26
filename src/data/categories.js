952e16b7be5523b33eaa2cb912c7adc4af93c148
aehamdy <ae.hamdy91@gmail.com>
2025-02-26 23:41:39 +0200

    Refactor: Implement usage of endPoints in categories for consistency

--- src/data/categories.js
+++ src/data/categories.js
@@ -6,17 +6,18 @@ import shoesCategory from "../assets/categories/shoes-category.webp";
 import fragrancesCategory from "../assets/categories/fragrances-category.webp";
 import watchesCategory from "../assets/categories/watches-category.webp";
 import beautyCategory from "../assets/categories/beauty-category.webp";
+import { endPoints } from "../config/apiConstants";
 
 
 const categories = [
-    {name: "dresses", image: dressesCategory, endPoint: `/womens-dresses`},
-    {name: "tops", image: topsCategory, endPoint: `/tops`},
-    {name: "bags", image: bagsCategory, endPoint: `/womens-bags`},
-    {name: "jewellery", image: jewelleryCateogry, endPoint: `/womens-jewellery`},
-    {name: "shoes", image: shoesCategory, endPoint: `/womens-shoes`},
-    {name: "fragrances", image: fragrancesCategory, endPoint: `/fragrances`},
-    {name: "watches", image: watchesCategory, endPoint: `/womens-watches`},
-    {name: "beauty", image: beautyCategory, endPoint: `/beauty`},
+    {name: "dresses", image: dressesCategory, endPoint: endPoints.womenDresses},
+    {name: "tops", image: topsCategory, endPoint: endPoints.womenTops},
+    {name: "bags", image: bagsCategory, endPoint: endPoints.womenBags},
+    {name: "jewellery", image: jewelleryCateogry, endPoint: endPoints.womenJewellery},
+    {name: "shoes", image: shoesCategory, endPoint: endPoints.womenShoes},
+    {name: "fragrances", image: fragrancesCategory, endPoint: endPoints.womenFragrances},
+    {name: "watches", image: watchesCategory, endPoint: endPoints.womenWatches},
+    {name: "beauty", image: beautyCategory, endPoint: endPoints.womenBeauty},
 ];
 
 export default categories;
