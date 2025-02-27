32311a55899593e79984b96ea6b1feff4ca05a8e
aehamdy <ae.hamdy91@gmail.com>
2025-02-27 23:32:49 +0200

    Refactor: Adjust LoaderCircular props

--- src/components/LoaderCircular.jsx
+++ src/components/LoaderCircular.jsx
@@ -1,6 +1,17 @@
-function LoaderCircular() {
+/* eslint-disable react/prop-types */
+
+function LoaderCircular({
+  loaderColor = "border-blue-500",
+  text,
+  textColor = "text-gray-300",
+}) {
   return (
-    <div className="w-3 h-3 rounded-full border-4 border-blue-600 border-r-transparent animate-spin"></div>
+    <div className="flex justify-center items-center gap-3 text-sm">
+      <div
+        className={`w-5 h-5 border-[3px] ${loaderColor} border-r-gray-300 rounded-full animate-spin`}
+      ></div>
+      {text && <p className={`${textColor}`}>{text}</p>}
+    </div>
   );
 }
 export default LoaderCircular;
