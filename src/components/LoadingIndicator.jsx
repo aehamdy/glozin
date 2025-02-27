28904c2bd77b1531d5a9ddc4b4fd5718ca749049
aehamdy <ae.hamdy91@gmail.com>
2025-02-27 20:11:37 +0200

    Create LoadingIndicator component that handles several loading components rendering

---
+++ src/components/LoadingIndicator.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import LoaderPipe from "./LoaderPipe";
+
+function LoadingIndicator({ loader, loaderSize = "large" }) {
+  const loaderValue = loaderSize.toLowerCase();
+  const size =
+    loaderValue === "small"
+      ? "w-4 h-4"
+      : loaderValue === "medium"
+      ? "w-6 h-6"
+      : loaderValue === "large" && "w-8 h-8";
+
+  return (
+    <div className="w-fit mx-auto">
+      {loader.toLowerCase() === "pipe" && <LoaderPipe />}
+    </div>
+  );
+}
+export default LoadingIndicator;
