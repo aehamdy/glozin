3d4cce2a77aef60765ee6e9fef2698b613ef7d5e
aehamdy <ae.hamdy91@gmail.com>
2025-02-27 20:34:35 +0200

    Refactor: Replaced structure with CheckoutButton component for better readability

--- src/components/CartDrawerPanel.jsx
+++ src/components/CartDrawerPanel.jsx
@@ -1,17 +1,9 @@
-import { Link } from "react-router-dom";
 import Subtotal from "./Subtotal";
-import { useCart } from "../context/CartContext";
-import { SET_BUY_NOW_PRODUCT, SET_SUBTOTAL } from "../constants/actionTypes";
 import { useCheckout } from "../context/CheckoutContext";
+import CheckoutButton from "./CheckoutButton";
 
 function CartDrawerPanel() {
-  const { dispatchCart, cartList } = useCart();
-  const { dispatchCheckout, subtotal } = useCheckout();
-
-  const handleOnClick = () => {
-    dispatchCart({ type: SET_BUY_NOW_PRODUCT, payload: null });
-    dispatchCheckout({ type: SET_SUBTOTAL, payload: cartList });
-  };
+  const { subtotal } = useCheckout();
 
   return (
     <div className="flex flex-col justify-between gap-6 mt-auto pt-4 pb-4 px-4 text-secondary-dark bg-cloud-gray border-t">
@@ -19,15 +11,7 @@ function CartDrawerPanel() {
         <div>Subtotal:</div>
         <Subtotal subtotalValue={subtotal} />
       </div>
-      <div className="flex flex-col items-center gap-5">
-        <Link
-          to="/checkout"
-          onClick={handleOnClick}
-          className=" w-3/4 py-3 font-semibold text-primary-light bg-secondary-dark hover:bg-primary-dark rounded-medium"
-        >
-          Checkout
-        </Link>
-      </div>
+      <CheckoutButton />
     </div>
   );
 }
