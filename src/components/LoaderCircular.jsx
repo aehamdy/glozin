9f09f848fd74d21a99078136459e7d6f945c46b1
aehamdy <ae.hamdy91@gmail.com>
2025-02-27 20:38:35 +0200

    Create LoaderCircular as a cricle shape loder

---
+++ src/components/LoaderCircular.jsx
@@ -0,0 +1,6 @@
+function LoaderCircular() {
+  return (
+    <div className="w-3 h-3 rounded-full border-4 border-blue-600 border-r-transparent animate-spin"></div>
+  );
+}
+export default LoaderCircular;
