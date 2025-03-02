8ec68a1657ee31cc4e1142add8bcaba64bc5ba22
aehamdy <ae.hamdy91@gmail.com>
2025-03-02 21:12:16 +0200

    Refactor: Replace card' keyword with 'skeleton' for consistency

--- src/components/SkeletonWrapper.jsx
+++ src/components/SkeletonWrapper.jsx
@@ -2,11 +2,11 @@
 import CategoryCardSkeleton from "./CategoryCardSkeleton";
 import ProductCardSkeleton from "./ProductCardSkeleton";
 
-function SkeletonWrapper({ type = "product-card" }) {
+function SkeletonWrapper({ type = "product-skeleton" }) {
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 h-fit gap-8">
       {Array.from({ length: 4 }).map((_, index) =>
-        type === "category-card" ? (
+        type === "category-skeleton" ? (
           <CategoryCardSkeleton key={index} />
         ) : (
           <ProductCardSkeleton key={index} />
