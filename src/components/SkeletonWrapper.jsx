fc25a48755e77b05dd37acf942cd9daee99adaee
aehamdy <ae.hamdy91@gmail.com>
2025-03-03 01:56:48 +0200

    Style: Add vertical and horizontal padding

--- src/components/SkeletonWrapper.jsx
+++ src/components/SkeletonWrapper.jsx
@@ -4,7 +4,7 @@ import ProductCardSkeleton from "./ProductCardSkeleton";
 
 function SkeletonWrapper({ type = "product-skeleton" }) {
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 h-fit gap-8">
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 h-fit gap-8 py-10 px-5">
       {Array.from({ length: 4 }).map((_, index) =>
         type === "category-skeleton" ? (
           <CategoryCardSkeleton key={index} />
