0cb8ff007eb32e873a0c60eeda57251130c86267
aehamdy <ae.hamdy91@gmail.com>
2025-03-02 23:59:51 +0200

    Feat: Implement loading state with skeleton for categories mocking api call

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import CategoriesWrapper from "../components/CategoriesWrapper";
 import SectionHeader from "../components/SectionHeader";
+import SkeletonWrapper from "../components/SkeletonWrapper";
 
 function Categories() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setIsLoading(false);
+    }, 2200);
+
+    return () => clearTimeout(timerId);
+  }, []);
+
   return (
     <section className="pt-vertical-spacing px-horizontal-spacing">
       <SectionHeader
         heading="Collections List"
         desc="Posuere in netus a eu varius adipiscing suspendisse elementum vitae temporsuspendisse ullamcorper aenean taciti morbi potenti."
       />
-      <CategoriesWrapper />
+      <div className="transition-opacity duration-500 ease-in-out">
+        {isLoading ? (
+          <SkeletonWrapper type="category-skeleton" />
+        ) : (
+          <CategoriesWrapper />
+        )}
+      </div>
     </section>
   );
 }
