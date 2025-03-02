3ead52584027ca9bcca172a18988486777cc6c1e
aehamdy <ae.hamdy91@gmail.com>
2025-03-02 20:53:32 +0200

    Create CategoryCardSkeleton to be displayed while fetching categories

---
+++ src/components/CategoryCardSkeleton.jsx
@@ -0,0 +1,8 @@
+function CategoryCardSkeleton() {
+  return (
+    <div className="relative h-[330px] bg-gray-200 rounded-tiny animate-pulse">
+      <div className="absolute bottom-2 md:bottom-3 end-1/2 translate-x-1/2 w-3/4 h-12 bg-gray-200 rounded-3xl"></div>
+    </div>
+  );
+}
+export default CategoryCardSkeleton;
