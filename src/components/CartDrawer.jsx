337e032a5d37f1d69e4dc997f3778ef70325a420
aehamdy <ae.hamdy91@gmail.com>
2025-03-05 21:18:03 +0200

    Style: Add sliding animation to CartDrawer

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import Modal from "../common/Modal";
 import { useCart } from "../context/CartContext";
 import CartDrawerContent from "./CartDrawerContent";
@@ -8,24 +9,37 @@ import Icon from "./Icon";
 
 function CartDrawer({ isCartOpen, handleCloseCart }) {
   const { cartList } = useCart();
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (isCartOpen) {
+      setIsVisible(true);
+      setTimeout(() => setIsAnimating(true), 10);
+    } else {
+      setIsAnimating(false);
+      setTimeout(() => setIsVisible(false), 300);
+    }
+  }, [isCartOpen]);
 
   const handleClick = (e) => {
     e.stopPropagation();
   };
 
-  return (
-    <Modal isOpen={isCartOpen} onClose={handleCloseCart}>
+  return isVisible ? (
+    <Modal isOpen={true} onClose={handleCloseCart}>
       <div
         onClick={handleClick}
-        className={`fixed top-0 right-0 w-10/12 md:w-3/5 lg:w-2/5 h-full flex flex-col pt-4 bg-primary-light rounded-l-small overflow-hidden z-top transition-transform duration-300 ease-in-out ${
-          isCartOpen ? "translate-x-0" : "translate-x-full"
-        }`}
+        className={`fixed top-0 end-0 w-10/12 md:w-3/5 lg:w-2/5 h-full flex flex-col pt-4 bg-primary-light rounded-l-small overflow-hidden z-top 
+            transition-transform duration-300 ease-in-out will-change-transform ${
+              isAnimating ? "translate-x-0" : "translate-x-full"
+            }`}
       >
         <div className="flex justify-between items-center px-4">
           <CartProductsCount />
           <Icon
             name="close"
-            className="text-content-medium-dark hover:scale-150"
+            className="text-content-medium-dark hover:rotate-180 duration-700"
             onClickFunction={handleCloseCart}
           />
         </div>
@@ -33,7 +47,7 @@ function CartDrawer({ isCartOpen, handleCloseCart }) {
         {cartList.length > 0 && <CartDrawerPanel />}
       </div>
     </Modal>
-  );
+  ) : null;
 }
 
 export default CartDrawer;
