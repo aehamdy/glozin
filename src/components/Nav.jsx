f95dba5ac2adf00c763800d26345c6bbad9e0682
aehamdy <ae.hamdy91@gmail.com>
2025-03-05 21:35:57 +0200

    Style: Add sliding animation to side nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,38 +3,54 @@ import Icon from "./Icon";
 import PanelTitle from "./PanelTitle";
 import NavList from "./NavList";
 import Modal from "../common/Modal";
+import { useEffect, useState } from "react";
 
 function Nav({ navVisibility, closeSideNav }) {
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
   const handleClick = (e) => {
     e.stopPropagation();
   };
 
+  useEffect(() => {
+    if (navVisibility) {
+      setIsVisible(true);
+      setTimeout(() => setIsAnimating(true), 10);
+    } else {
+      setIsAnimating(false);
+      setTimeout(() => setIsVisible(false), 300);
+    }
+  }, [navVisibility]);
+
   return (
     <>
       {/* For small and medium screens */}
-      <Modal isOpen={navVisibility} onClose={closeSideNav}>
-        <nav
-          onClick={handleClick}
-          className={`lg:hidden fixed top-0 left-0 flex-col items-start w-2/3 h-full bg-primary-light z-top rounded-r-small overflow-hidden transition-all duration-[350ms] ease-in-out transform ${
-            navVisibility ? "translate-x-0" : "-translate-x-full"
-          }`}
-          role="menu"
-          aria-expanded={navVisibility}
-        >
-          <div className="flex flex-col">
-            <div className="flex justify-between items-center py-4 px-3 bg-primary-red">
-              <PanelTitle title="menu" styles="text-primary-light" />
-              <Icon
-                name="close"
-                className="text-primary-light hover:scale-150"
-                onClickFunction={closeSideNav}
-              />
-            </div>
+      {isVisible ? (
+        <Modal isOpen={true} onClose={closeSideNav}>
+          <nav
+            onClick={handleClick}
+            className={`lg:hidden fixed top-0 start-0 flex-col items-start w-2/3 h-full bg-primary-light z-top rounded-r-small overflow-hidden 
+              transition-transform duration-300 ease-in-out will-change-transform 
+              ${isAnimating ? "translate-x-0" : "-translate-x-full"}`}
+            role="menu"
+            aria-expanded={navVisibility}
+          >
+            <div className="flex flex-col">
+              <div className="flex justify-between items-center py-4 px-3 bg-primary-red">
+                <PanelTitle title="menu" styles="text-primary-light" />
+                <Icon
+                  name="close"
+                  className="text-primary-light hover:scale-150"
+                  onClickFunction={closeSideNav}
+                />
+              </div>
 
-            <NavList navVisibility={navVisibility} />
-          </div>
-        </nav>
-      </Modal>
+              <NavList navVisibility={navVisibility} />
+            </div>
+          </nav>
+        </Modal>
+      ) : null}
 
       {/* For large screens */}
       <nav role="menu" className="hidden lg:block">
