502919fdafa063bc73d51a1047ae6cc138e2af93
aehamdy <ae.hamdy91@gmail.com>
2025-03-06 16:30:35 +0200

    Refactor: Implement showToast function right after adding/removing item to/from wishlist

--- src/components/WishlistButton.jsx
+++ src/components/WishlistButton.jsx
@@ -4,11 +4,14 @@ import { useFetchSingleProduct } from "../hooks/useFetchSingleProduct";
 import { useWishlist } from "../context/wishlistContext";
 import { useEffect, useState } from "react";
 import ProductCardButton from "./ProductCardButton";
+import { useToast } from "../context/ToastContext";
+import toastMessages from "../data/toastMessages";
 
 function WishlistButton({ productId, variant, icon, tooltipValue }) {
   const { addToWishlist, removeFromWishlist, wishlistProducts } = useWishlist();
   const { product } = useFetchSingleProduct(productId);
   const [isInWishlist, setIsInWishlist] = useState(false);
+  const { showToast } = useToast();
 
   useEffect(() => {
     if (product) {
@@ -30,8 +33,10 @@ function WishlistButton({ productId, variant, icon, tooltipValue }) {
     if (product) {
       if (isInWishlist) {
         removeFromWishlist(productId);
+        showToast(toastMessages.removeFromWishlist);
       } else {
         addToWishlist(product);
+        showToast(toastMessages.addToWishlist);
       }
     }
   };
