60f1b4a771b943dab8db2b979d6abaa0cca3ab6d
aehamdy <ae.hamdy91@gmail.com>
2025-03-06 08:43:30 +0200

    Create toastMessages object that contains all toast notification messages for consistency

---
+++ src/data/toastMessages.js
@@ -0,0 +1,7 @@
+const toastMessages = {
+  addToCart: "🛒 Item added to cart!",
+  addToWishlist: "❤️ Saved to wishlist!",
+  removeFromWishlist: "💔 Removed from wishlist!",
+};
+
+export default toastMessages;
