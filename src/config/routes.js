db4ef7584632b505646be65c1747e3d9c14f8c72
aehamdy <ae.hamdy91@gmail.com>
2025-03-10 22:22:48 +0200

    Refactor: Add new path name for category products

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,6 +3,7 @@ const ROUTES = {
     SHOP: "/shop",
     PRODUCT_DETAILS: "/product/:id",
     CATEGORIES: "/categories",
+    CATEGORY_PRODUCT: "/categories/:categoryName",
     SALE: "/sale",
     WISHLIST: "/wishlist",
     ABOUT: "/about",
