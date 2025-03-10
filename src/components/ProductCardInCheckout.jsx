29c6a8a4791a3c0033265189da89e0294168478b
aehamdy <ae.hamdy91@gmail.com>
2025-03-10 22:48:56 +0200

    Style: Add height for product images for consistency

--- src/components/ProductCardInCheckout.jsx
+++ src/components/ProductCardInCheckout.jsx
@@ -10,13 +10,17 @@ function ProductCardInCheckout({ product }) {
   return (
     <article className="flex justify-between gap-3 text-sm md:text-base">
       <div className="flex gap-3">
-        <div className="relative w-[70px] border border-gray-300 rounded-md">
+        <div className="relative w-[70px] h-[70px] flex justify-center items-center border border-gray-300 rounded-md">
           {!buyNowProduct && (
-            <span className="flex justify-center  items-center absolute top-0 end-0 translate-x-1/2 -translate-y-1/2 w-5 h-5  text-xs text-white bg-content-light-dark rounded-full">
+            <span className="flex justify-center items-center absolute top-0 end-0 translate-x-1/2 -translate-y-1/2 w-5 h-5  text-xs text-white bg-content-light-dark rounded-full">
               {product.orderQuantity}
             </span>
           )}
-          <img src={`${product.images[0]}`} alt={`${product.title} image`} />
+          <img
+            src={`${product.images[0]}`}
+            alt={`${product.title} image`}
+            className="h-full object-cover"
+          />
         </div>
 
         <div className="flex flex-col items-start">
