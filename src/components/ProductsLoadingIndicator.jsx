0dbb0b28c192328f600ed5f63c21ac400d6b88ef
aehamdy <ae.hamdy91@gmail.com>
2025-03-10 22:34:36 +0200

    Refactor: Create 'categories' folder for categories images

--- src/components/ProductsLoadingIndicator.jsx
+++
@@ -1,8 +0,0 @@
-function ProductsLoadingIndicator() {
-  return (
-    <h3 className="text-center text-lg md:text-xl text-content-medium-dark">
-      Loading products...
-    </h3>
-  );
-}
-export default ProductsLoadingIndicator;
