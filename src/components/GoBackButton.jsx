fc4fae3cd5307c6ce668c257d184662285cc6906
aehamdy <ae.hamdy91@gmail.com>
2025-03-11 22:20:36 +0200

    Refactor: Implement useNavigate hook to go one step back easily when click on the button

--- src/components/GoBackButton.jsx
+++ src/components/GoBackButton.jsx
@@ -1,17 +1,19 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Icon from "./Icon";
 
 function GoBackButton() {
+  const navigate = useNavigate();
+
   return (
-    <Link
-      to="/"
+    <button
+      onClick={() => navigate(-1)}
       className="group flex items-center justify-center w-fit bg-primary-light hover:bg-secondary-dark p-1.5 border rounded-tiny duration-medium"
     >
       <Icon
         name="leftArrow"
         className="text-secondary-dark group-hover:text-primary-light"
       />
-    </Link>
+    </button>
   );
 }
 export default GoBackButton;
