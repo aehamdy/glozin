b99f303424b91a6e2f50139f81b9acfa70b80ea8
aehamdy <ae.hamdy91@gmail.com>
2025-03-16 23:17:42 +0200

    Add springy fade-in animation using motion library

--- src/components/HeroSlideContent.jsx
+++ src/components/HeroSlideContent.jsx
@@ -2,10 +2,25 @@
 import Button from "./Button";
 import HeroSlideHeading from "./HeroSlideHeading";
 import HeroSlideSubheading from "./HeroSlideSubheading";
+import { motion } from "motion/react";
+
+const containerVariants = {
+  hidden: { y: "100%", opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { type: "spring", damping: 8, duration: 1, delay: 0.8 },
+  },
+};
 
 function HeroSlideContent({ index, slides, currentSlide }) {
   return (
-    <div className="absolute bottom-12 start-4 flex flex-col items-start gap-3 text-primary-light">
+    <motion.div
+      className="absolute bottom-12 start-4 flex flex-col items-start gap-3 text-primary-light"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <HeroSlideSubheading
         index={index}
         slides={slides}
@@ -20,7 +35,7 @@ function HeroSlideContent({ index, slides, currentSlide }) {
         value="Shop Collection"
         className="w-fit mt-4 lg:mt-6 py-3 md:py-3 lg:py-4 px-6 md:px-6 lg:px-9 font-semibold text-[15px] text-primary-dark hover:text-primary-light bg-primary-light hover:bg-secondary-dark rounded-medium duration-medium"
       />
-    </div>
+    </motion.div>
   );
 }
 export default HeroSlideContent;
