04b201a34bf868a907755acefb25802bfa620029
aehamdy <ae.hamdy91@gmail.com>
2025-03-16 17:54:03 +0200

    Refactor: Add font size clamp function in tailwind config file, and implement it in TopBar

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,13 +1,26 @@
+/* eslint-disable react/prop-types */
+import discountCodes from "../data/discountCodes";
 import Icon from "./Icon";
 
-/* eslint-disable react/prop-types */
 function TopBar({ hideAnnouncementBar }) {
+  const selectedOffer = discountCodes.find(
+    (offer) => offer.label === "white friday"
+  );
+
+  const formattedLabel = selectedOffer.label
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
+  const offerCode = selectedOffer.code.toUpperCase();
   return (
     <div
-      className={`absolute w-full start-0 top-0 flex gap-4 justify-center items-center py-1 text-sm text-primary-light bg-muted-blue`}
+      className={`absolute w-full start-0 top-0 flex gap-4 justify-center items-center py-1 text-clamp-topbar text-primary-light bg-muted-blue`}
     >
       <p className="">
-        🔥 Don&apos;t miss our White Friday <span>Sale 75% Off</span>
+        🔥 Don&apos;t miss our {formattedLabel}{" "}
+        <span>Sale {selectedOffer.discountAmount}% Off</span> using{" "}
+        <span className="font-semibold">{offerCode}</span>
       </p>
       <Icon
         name="close"
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,65 +1,66 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       spacing: {
-        'vertical-spacing': '5.625rem', //90px
-        'horizontal-spacing': '1.066rem', //15px
-        heroHeightSmall: 'calc(100dvh - 170px)',
-        heroHeightMedium: 'calc(100dvh - 170px)',
+        "vertical-spacing": "5.625rem", //90px
+        "horizontal-spacing": "1.066rem", //15px
+        heroHeightSmall: "calc(100dvh - 170px)",
+        heroHeightMedium: "calc(100dvh - 170px)",
+      },
+      fontSize: {
+        "clamp-topbar": "clamp(0.5rem, 2.5vw, 0.8rem)",
       },
       colors: {
-        'muted-blue': '#607890',
-        'overlay': '#00000098',
-        'navigation-dots': '#ffffff99',
-        'cloud-gray': '#F5F5F5',
-        'rate-color': '#ff9c0f',
+        "muted-blue": "#607890",
+        overlay: "#00000098",
+        "navigation-dots": "#ffffff99",
+        "cloud-gray": "#F5F5F5",
+        "rate-color": "#ff9c0f",
         primary: {
-          dark: '#000',
-          light: '#ffffff',
-          red: '#d0473d',
-          border: '#ebebeb',
+          dark: "#000",
+          light: "#ffffff",
+          red: "#d0473d",
+          border: "#ebebeb",
         },
         secondary: {
-          dark: '#111',
+          dark: "#111",
           // light: '#ebebeb',
-          light: '#c0c0c0',
-          red: '#d0473e',
+          light: "#c0c0c0",
+          red: "#d0473e",
         },
         button: {
-          buyNow: '#d0473e',
+          buyNow: "#d0473e",
         },
         price: {
-          originalPrice: '#666',
-          priceInCart: '#111',
-          salePrice: '#d0473e',
+          originalPrice: "#666",
+          priceInCart: "#111",
+          salePrice: "#d0473e",
         },
         heading: {
-          dark: '#111',
+          dark: "#111",
         },
         content: {
-          dark: '#111',
-          'medium-dark': '#444',
-          'light-dark': '#666',
+          dark: "#111",
+          "medium-dark": "#444",
+          "light-dark": "#666",
         },
         badge: {
-          notification: '#d0473d',
-          sale: '#d0473e',
-          limitedStock: '#14854f',
-        }
+          notification: "#d0473d",
+          sale: "#d0473e",
+          limitedStock: "#14854f",
+        },
       },
       borderRadius: {
-        tiny: '0.5rem',
-        small: '1rem',
-        medium: '2rem',
+        tiny: "0.5rem",
+        small: "1rem",
+        medium: "2rem",
       },
       zIndex: {
-        overlay: '95',
-        top: '100',
+        overlay: "95",
+        top: "100",
       },
       transitionDuration: {
         short: "250ms",
@@ -67,17 +68,17 @@ export default {
         long: "700ms",
       },
       boxShadow: {
-        'order-details': '0px 0px 40px 5px rgba(0,0,0,0.25)',
+        "order-details": "0px 0px 40px 5px rgba(0,0,0,0.25)",
       },
       keyframes: {
-        'horizontal-text-move': {
-          to: {transform: 'translateX(-33.33%)'}
-        }
+        "horizontal-text-move": {
+          to: { transform: "translateX(-33.33%)" },
+        },
       },
       animation: {
-        'horizontal-move': 'horizontal-text-move 20s linear infinite',
-      }
+        "horizontal-move": "horizontal-text-move 20s linear infinite",
+      },
     },
   },
   plugins: [],
-}
+};
