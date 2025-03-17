a4152ee64ef97824c93393fce44c9f9eeb3f07ed
aehamdy <ae.hamdy91@gmail.com>
2025-03-18 00:58:57 +0200

    Add fade-in animation

--- src/components/HeadlineText.jsx
+++ src/components/HeadlineText.jsx
@@ -1,7 +1,18 @@
 /* eslint-disable react/prop-types */
+import { motion } from "motion/react";
+import { fadeInAnimation } from "../animations/variants";
+
 function HeadlineText({ text }) {
   return (
-    <div className="mb-3 font-semibold text-sm text-secondary-dark">{text}</div>
+    <motion.div
+      className="mb-3 font-semibold text-sm text-secondary-dark"
+      variants={fadeInAnimation("down", 0.3)}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: false, amount: 0.3 }}
+    >
+      {text}
+    </motion.div>
   );
 }
 export default HeadlineText;
