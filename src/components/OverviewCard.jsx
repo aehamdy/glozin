582f36f8c051a43304ca2eb6078d56e3322acb71
aehamdy <ae.hamdy91@gmail.com>
2025-03-18 00:25:13 +0200

    Style: Add fade-in animation

--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/prop-types */
+import { motion } from "motion/react";
 import Button from "./Button";
+import { fadeInAnimation } from "../animations/variants";
 
 function OverviewCard({ item, index }) {
   return (
@@ -8,11 +10,23 @@ function OverviewCard({ item, index }) {
         (index + 1) % 2 === 0 && "md:flex-row-reverse"
       } items-center gap-8`}
     >
-      <div className="md:w-1/2 rounded-small overflow-hidden">
+      <motion.div
+        className="md:w-1/2 rounded-small overflow-hidden"
+        variants={fadeInAnimation(index % 2 === 0 ? "down" : "up")}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: false, amount: 0.3 }}
+      >
         <img src={item.image} alt="overview image" className="object-fit" />
-      </div>
+      </motion.div>
 
-      <div className="flex flex-col gap-3 lg:gap-4 md:w-1/2 text-secondary-dark">
+      <motion.div
+        className="flex flex-col gap-3 lg:gap-4 md:w-1/2 text-secondary-dark"
+        variants={fadeInAnimation(index % 2 === 0 ? "left" : "right")}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: false, amount: 0.3 }}
+      >
         <div className="space-y-2">
           <div className="font-semibold text-sm">{item.heading}</div>
           <h2 className="font-semibold text-3xl">{item.subheading}</h2>
@@ -25,7 +39,7 @@ function OverviewCard({ item, index }) {
           value={item.buttonText}
           className="w-fit mx-auto py-3 px-6 font-medium text-md hover:text-lg text-primary-light bg-secondary-dark hover:bg-primary-dark rounded-medium duration-short"
         />
-      </div>
+      </motion.div>
     </article>
   );
 }
