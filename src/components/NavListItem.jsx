11ee4d1808972dbc78bed811f1ca13f4804a3574
aehamdy <ae.hamdy91@gmail.com>
2025-03-18 21:41:16 +0200

    Style: Add hover effect animation to NavListItem

--- src/components/NavListItem.jsx
+++ src/components/NavListItem.jsx
@@ -1,6 +1,12 @@
 /* eslint-disable react/prop-types */
+import { motion } from "motion/react";
 import { Link } from "react-router-dom";
 import NavItemBadge from "./NavItemBadge";
+
+const divVariants = {
+  hidden: { x: 0 },
+  visible: { x: 40, transition: { duration: 0.2 } },
+};
 
 function NavListItem({ item }) {
   const capitalizedLabel =
@@ -12,19 +18,26 @@ function NavListItem({ item }) {
 
   return (
     <li className="flex w-full">
-      <Link
-        to={item.url}
-        className={`relative before:absolute before:start-0 before:bottom-0 before:h-[2px] before:w-0
+      <motion.div
+        className="w-full"
+        variants={divVariants}
+        initial="hidden"
+        whileHover="visible"
+      >
+        <Link
+          to={item.url}
+          className={`relative before:absolute before:start-0 before:bottom-0 before:h-[2px] before:w-0
         lg:hover:before:w-full before:bg-secondary-red flex items-center gap-5 lg:gap-0 w-full lg:p-2 font-semibold py-4 lg:py-1
       ${
         isSaleCategory
           ? "text-primary-red hover:text-secondary-red"
           : "text-secondary-dark hover:text-secondary-red"
       } lg:border-none duration-medium before:duration-medium`}
-      >
-        <div className="">{capitalizedLabel}</div>
-        {item.badge && <NavItemBadge badgeText={item.badge} />}
-      </Link>
+        >
+          <div className="">{capitalizedLabel}</div>
+          {item.badge && <NavItemBadge badgeText={item.badge} />}
+        </Link>
+      </motion.div>
     </li>
   );
 }
