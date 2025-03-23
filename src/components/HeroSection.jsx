89146a66003ca59020f058cea2f06dc1770db59e
aehamdy <ae.hamdy91@gmail.com>
2025-03-24 00:26:35 +0200

    Style: Add delay to the animation

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -35,7 +35,7 @@ const containerVariants = {
   visible: {
     x: 0,
     opacity: 1,
-    transition: { type: "spring", duration: 1.5 },
+    transition: { type: "spring", duration: 1.5, delay: 0.8 },
   },
 };
 
