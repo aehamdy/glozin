9cc0585224cb8b8b18eab4a791ea41403d07dc2b
aehamdy <ae.hamdy91@gmail.com>
2025-03-24 00:12:49 +0200

    Refactor: Add navigation buttons

--- src/components/TopCollectionsSection.jsx
+++ src/components/TopCollectionsSection.jsx
@@ -1,5 +1,6 @@
 import SectionHeader from "./SectionHeader";
 import CategoriesWrapper from "./CategoriesWrapper";
+import Icon from "./Icon";
 
 function TopCollectionsSection() {
   return (
@@ -8,6 +9,17 @@ function TopCollectionsSection() {
         heading="Top Collections"
         desc="Express your style with our standout collection—fashion meets sophistication."
       />
+
+      {/* Navigation buttons */}
+      <div className="lg:hidden flex justify-between items-center cursor-not-allowed">
+        <button className="p-2 text-secondary-dark hover:text-primary-light bg-gray-50 hover:bg-secondary-dark rounded-full duration-100 cursor-pointer">
+          <Icon name="leftArrow" />
+        </button>
+        <button className="p-2 text-secondary-dark hover:text-primary-light bg-gray-50 hover:bg-secondary-dark rounded-full duration-100 cursor-pointer">
+          <Icon name="rightArrow" />
+        </button>
+      </div>
+
       <CategoriesWrapper variant="circle" />
     </section>
   );
