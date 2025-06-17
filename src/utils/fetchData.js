cbb47afc753b609c97a6cf3a964c7dc3c5dc1f12
AhmedEssam <ae.devverse@gmail.com>
2025-06-17 15:00:47 +0300

    Chore: Update

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,11 +1,12 @@
 async function fetchData(endpoint, setState) {
   try {
-    const response = await fetch(`https://dummyjson.com/products/category/${endpoint}`);
+    const response = await fetch(
+      `https://dummyjson.com/products/category/${endpoint}`
+    );
     const data = await response.json();
 
     setState(data.products || []);
-
-  } catch(err) {
+  } catch (err) {
     console.error("Error fetching data: ", err);
   }
 }
