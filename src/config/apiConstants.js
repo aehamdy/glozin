f3736727822202de351b0d6259c0bcf19589e28b
AhmedEssam <ae.devverse@gmail.com>
2025-06-17 16:36:29 +0300

    Chore: Update api constants

--- src/config/apiConstants.js
+++ src/config/apiConstants.js
@@ -1,14 +1,16 @@
+import ROUTES from "./routes";
+
 export const BASE_API_URL = import.meta.env.VITE_BASE_API_URL;
 
 export const productCategoryKey = "/products/category";
 
 export const endPoints = {
-  womenBeauty: "/beauty",
-  womenDresses: "/womens-dresses",
-  womenFragrances: "/fragrances",
-  womenTops: "/tops",
-  womenBags: "/womens-bags",
-  womenJewellery: "/womens-jewellery",
-  womenShoes: "/womens-shoes",
-  womenWatches: "/womens-watches",
+  womenBeauty: `${ROUTES.CATEGORIES}/beauty`,
+  womenDresses: `${ROUTES.CATEGORIES}/womens-dresses`,
+  womenFragrances: `${ROUTES.CATEGORIES}/fragrances`,
+  womenTops: `${ROUTES.CATEGORIES}/tops`,
+  womenBags: `${ROUTES.CATEGORIES}/womens-bags`,
+  womenJewellery: `${ROUTES.CATEGORIES}/womens-jewellery`,
+  womenShoes: `${ROUTES.CATEGORIES}/womens-shoes`,
+  womenWatches: `${ROUTES.CATEGORIES}/womens-watches`,
 };
