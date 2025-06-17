69fccc6fd96a85275ce4df4f7d279aeaee783bb4
AhmedEssam <ae.devverse@gmail.com>
2025-06-17 16:30:28 +0300

    Chore: Update the link of each slide

--- src/data/heroSlides.js
+++ src/data/heroSlides.js
@@ -2,7 +2,6 @@ import { endPoints } from "../config/apiConstants";
 import image1 from "../assets/hero-image-1.jpg";
 import image2 from "../assets/hero-image-2.jpg";
 import image3 from "../assets/hero-image-3.jpg";
-import ROUTES from "../config/routes";
 
 export const heroSlides = [
   {
@@ -10,20 +9,20 @@ export const heroSlides = [
     image: image1,
     subHeading: "Dress Collection",
     heading: "Online Exclusive",
-    link: `${ROUTES.CATEGORIES}${endPoints.womenDresses}`,
+    link: endPoints.womenDresses,
   },
   {
     id: 2,
     image: image2,
     subHeading: "Look Exclusive",
     heading: "High-Top Design",
-    link: `${ROUTES.CATEGORIES}${endPoints.womenTops}`,
+    link: endPoints.womenTops,
   },
   {
     id: 3,
     image: image3,
     subHeading: "Season Collection",
     heading: "Trendy Bags",
-    link: `${ROUTES.CATEGORIES}${endPoints.womenBags}`,
+    link: endPoints.womenBags,
   },
 ];
