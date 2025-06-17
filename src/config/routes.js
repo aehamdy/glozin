c06e483d7831819bad7a5d0cc2374672c590d9c3
AhmedEssam <ae.devverse@gmail.com>
2025-06-17 14:51:04 +0300

    Adjust ROUTES file

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,17 +1,17 @@
 const ROUTES = {
-    HOME: "/",
-    SHOP: "/shop",
-    PRODUCT_DETAILS: "/product/:id",
-    CATEGORIES: "/categories",
-    CATEGORY_PRODUCT: "/categories/:categoryName",
-    SALE: "/sale",
-    WISHLIST: "/wishlist",
-    ABOUT: "/about",
-    CONTACT: "/contact",
-    OUR_STORES: "/ourstores",
-    HELP: "/help",
-    CHECKOUT: "/checkout",
-    ORDER_CONFIRMATION: "/order-confirmation",
-}
+  HOME: "/",
+  SHOP: "/shop",
+  PRODUCT_DETAILS: "/product/:id",
+  CATEGORIES: "/categories",
+  CATEGORY_PRODUCT: "/categories/:categoryName",
+  SALE: "/sale",
+  WISHLIST: "/wishlist",
+  ABOUT: "/about",
+  CONTACT: "/contact",
+  OUR_STORES: "/ourstores",
+  HELP: "/help",
+  CHECKOUT: "/checkout",
+  ORDER_CONFIRMATION: "/order-confirmation",
+};
 
 export default ROUTES;
