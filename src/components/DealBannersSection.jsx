f7183d701eb7d6b9e84d8ba40491fea8c921c805
AhmedEssam <ae.devverse@gmail.com>
2025-06-17 16:45:59 +0300

    Chore: Cleanup and use DealBanner instead and get rid of hard coded code

--- src/components/DealBannersSection.jsx
+++ src/components/DealBannersSection.jsx
@@ -1,46 +1,12 @@
-import { motion } from "motion/react";
-import dealBanner1 from "../assets/deal-banner-1.webp";
-import dealBanner2 from "../assets/deal-banner-2.webp";
-import BannerContent from "./BannerContent";
-import BannerImage from "./BannerImage";
-import { fadeInAnimation } from "../animations/variants";
-
-const deals = [
-  {
-    id: 1,
-    image: dealBanner1,
-    mainText: "Save! 30-50%Off",
-    subText: "Today-White friday",
-    button: "Shop Save",
-  },
-  {
-    id: 2,
-    image: dealBanner2,
-    mainText: "High-Top Design",
-    subText: "Save 30%-50% Clothing",
-    button: "Shop Save",
-  },
-];
+import { deals } from "../data/dealBanners";
+import DealBanner from "./DealBanner";
 
 function DealBannersSection() {
   return (
     <section className="mb-vertical-spacing mx-horizontal-spacing">
       <div className="grid gridcols-1 md:grid-cols-2 justify-between gap-7">
         {deals.map((deal, index) => (
-          <motion.div
-            key={index}
-            className="relative flex flex-col rounded-xl overflow-hidden cursor-pointer"
-            variants={fadeInAnimation(index % 2 === 0 ? "right" : "left")}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: false, amount: 0.4 }}
-          >
-            <BannerImage image={deal.image} />
-            <BannerContent
-              content={deal}
-              mainTextStyle="text-3xl md:text-3xl lg:text-2xl"
-            />
-          </motion.div>
+          <DealBanner key={deal.id} index={index} deal={deal} />
         ))}
       </div>
     </section>
