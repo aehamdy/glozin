719da1c02607f1e2d47bee683cf835831a0623cf
AhmedEssam <ae.devverse@gmail.com>
2025-06-17 16:28:50 +0300

    Chore: Cleanup and implement usage of spotlight banners

--- src/components/SpotlightOfferBanner.jsx
+++ src/components/SpotlightOfferBanner.jsx
@@ -3,21 +3,26 @@ import { motion } from "motion/react";
 import BannerContent from "./BannerContent";
 import BannerImage from "./BannerImage";
 import { fadeInAnimation } from "../animations/variants";
+import { Link } from "react-router-dom";
 
 function SpotlightOfferBanner({ banner, index }) {
   return (
     <motion.article
-      className="group relative flex flex-col text-primary-light rounded-small overflow-hidden cursor-pointer"
       variants={fadeInAnimation("up", index * 0.2)}
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true, amount: 0.15 }}
     >
-      <BannerImage image={banner.image} />
-      <BannerContent
-        content={banner}
-        mainTextStyle="text-3xl md:text-3xl lg:text-2xl"
-      />
+      <Link
+        to={banner.link}
+        className="group relative flex flex-col text-primary-light rounded-small overflow-hidden cursor-pointer"
+      >
+        <BannerImage image={banner.image} />
+        <BannerContent
+          content={banner}
+          mainTextStyle="text-3xl md:text-3xl lg:text-2xl"
+        />
+      </Link>
     </motion.article>
   );
 }
--- src/components/SpotlightOffersSection.jsx
+++ src/components/SpotlightOffersSection.jsx
@@ -1,36 +1,10 @@
+import { spotlightBanners } from "../data/spotlightBanners";
 import SpotlightOfferBanner from "./SpotlightOfferBanner";
-import spotlightImage1 from "../assets/spotlight-offer-image-1.jpg";
-import spotlightImage2 from "../assets/spotlight-offer-image-2.jpg";
-import spotlightImage3 from "../assets/spotlight-offer-image-3.jpg";
-
-const banners = [
-  {
-    id: 1,
-    image: spotlightImage1,
-    mainText: "Colour Spotlight",
-    subText: "Save 30-50% Blazers",
-    text: "In-store! Limited time offer",
-  },
-  {
-    id: 2,
-    image: spotlightImage2,
-    mainText: "Coats Collection",
-    subText: "Sale up to 75% off",
-    text: "From $175. Do not miss!",
-  },
-  {
-    id: 3,
-    image: spotlightImage3,
-    mainText: "Everyday Luxury",
-    subText: "Save 10-30% Dresses",
-    text: "In-store! Limited time offer",
-  },
-];
 
 function SpotlightOffersSection() {
   return (
     <section className="grid grid-col-1 md:grid-cols-3 justify-items-center gap-4 lg:gap-8 mb-vertical-spacing mx-horizontal-spacing">
-      {banners.map((banner, index) => (
+      {spotlightBanners.map((banner, index) => (
         <SpotlightOfferBanner key={banner.id} index={index} banner={banner} />
       ))}
     </section>
